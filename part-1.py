8d38c7cb786b7bbe1ee74120a07461358d61583c
mairandomness <mairakodama@gmail.com>
2021-05-14 12:23:28 -0700

    Add solutions

--- part-1.py
+++ part-1.py
@@ -4,17 +4,34 @@
 # the appropriate comment.
 
 # factorial
-
+def factorial(n):
+    if n < 0:
+        raise ValueError
+    elif n == 0:
+        return 1
+    return factorial(n-1) * n
 
 
 # reverse
-
+def reverse(text):
+    if text == "":
+        return text
+    return reverse(text[1:]) + text[0]
 
 
 # bunny
+def bunny(count):
+    if count == 0:
+        return 0
+    return bunny(count-1) + 2
 
 
-
-# is_nested_parens
-
-
+# is_nested_parens in the no new string version
+def is_nested_parens(text, loops=0):
+    i = loops
+    j = len(text) - 1 - loops
+    if i == j + 1:
+        return True
+    elif text[i] != "(" or text[j] != ")":
+        return False
+    return is_nested_parens(text, loops + 1)
--- part-2.py
+++ part-2.py
@@ -4,13 +4,36 @@
 # the appropriate comment.
 
 # search
+def search(item_list, item):
+    if not item_list:
+        return False
+
+    cur = item_list.pop()
+    if cur == item:
+        return True
+
+    return search(item_list, item)
 
 
-
-# is_palindrome
-
+# is_palindrome (to solve without creating new strings, we could do something like
+# what was done in part1 is_nested_parens)
+def is_palindrome(text):
+    if not text:
+        return True
+    
+    elif text[0] != text[-1]:
+        return False
+    
+    return is_palindrome(text[1:-1])
 
 
 # digit_match
-
+def digit_match(num1, num2):
+    if num1 < 10 or num2 < 10:
+        if num1 % 10 == num2 % 10:
+            return 1
+        return 0
+    elif num1 % 10 == num2 % 10:
+        return 1 + digit_match(num1//10, num2//10)
+    return digit_match(num1//10, num2//10)
 
