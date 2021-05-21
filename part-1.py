60da5c7778dc4c4daa608d7873764e503e0858e9
kmalakhova <kgrekhova@gmail.com>
2021-05-21 13:12:12 -0700

    Add recursion prac

--- part-1.py
+++ part-1.py
@@ -4,17 +4,40 @@
 # the appropriate comment.
 
 # factorial
-
+def factorial(n):
+    if n == -1:
+        raise ValueError
+    if n == 0:
+        return 1
+    if n == 1:
+        return 1
+    return n * factorial(n-1)
 
 
 # reverse
-
+def reverse(text):
+    if len(text) <= 1:
+        return text
+    f_char = text[0]
+    leftovers = text[1:]
+    return reverse(leftovers) + f_char
 
 
 # bunny
-
+def bunny(count):
+    if count == 0:
+        return 0
+    if count == 1:
+        return 2
+    return 2 + bunny(count-1)
 
 
 # is_nested_parens
-
+def is_nested_parens(parens):
+    if len(parens) == 0:
+        return True
+    elif parens[:1] != "(" or parens[-1:] != ")":
+        return False
+    else:
+        return is_nested_parens(parens[1:-1])
 
