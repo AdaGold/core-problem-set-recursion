f26c93fc9976953a1c23a6c033a32e2eb63259d9
halisa <halisasaipulaa@gmail.com>
2021-05-23 23:10:47 -0700

    recursion solutions

--- part-1.py
+++ part-1.py
@@ -4,17 +4,38 @@
 # the appropriate comment.
 
 # factorial
+def factorial(num):
+    if num == 0:
+        return 1
+    if num <0:
+        raise ValueError
 
+    return num * factorial(num - 1)
 
 
 # reverse
-
+def reverse(s):
+    if len(s)==0:
+        return s
+    return reverse(s[1:]) + s[0]
 
 
 # bunny
-
+def bunny(n):
+    if n == 0 :
+        return 0
+    return bunny(n-1) +2
 
 
 # is_nested_parens
+def is_nested_parens(s):
+    open_s = "("
+    close_s = ")"
+    if len(s)<1:
+        return True
+        
+    elif s[0]!=open_s or s[-1]!=close_s:
+        return False
+    return is_nested_parens(s[1:-1])
 
 
--- part-2.py
+++ part-2.py
@@ -4,11 +4,21 @@
 # the appropriate comment.
 
 # search
-
-
+def search(array,query):
+    if not array :
+        return False
+    elif array[0] == query:
+        return True
+    return search(array[1:],query)
 
 # is_palindrome
-
+def is_palindrome(s):
+    if len(s)<=1:
+        return True
+    elif s[0] != s[-1]:
+        return False
+    else:
+        return is_palindrome(s[1:-1]) 
 
 
 # digit_match
