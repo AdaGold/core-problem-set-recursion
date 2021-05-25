b1e87da70b00a051c80bf0f9eeb25f4db75f8bda
Whit Sundby <wksundby@gmail.com>
2021-05-25 10:40:46 -0700

    adds solutions to recursion prompts

--- part-1.py
+++ part-1.py
@@ -5,16 +5,34 @@
 
 # factorial
 
+def factorial(num):
+    if num < 0:
+        raise ValueError
+    if num == 1 or num == 0:
+        return 1
+    return num * factorial(num - 1)
 
 
 # reverse
 
-
+def reverse(text):
+    if text == "":
+        return ""
+    next_letter = text[len(text)-1]
+    return next_letter + reverse(text[0:-1])
 
 # bunny
 
-
+def bunny(count):
+    if count == 0:
+        return 0
+    return 2 + bunny(count - 1)
 
 # is_nested_parens
 
-
+def is_nested_parens(str):
+    if str == "":
+        return True
+    if str[0] + str[-1] != "()":
+        return False
+    return is_nested_parens(str[1:-1])
--- part-2.py
+++ part-2.py
@@ -5,12 +5,29 @@
 
 # search
 
-
+def search(array, query):
+    if array == []:
+        return False
+    if array[len(array)-1] == query:
+        return True
+    array.pop()
+    return search(array, query)
 
 # is_palindrome
 
-
+def is_palindrome(str):
+    if len(str) < 2:
+        return True
+    if str[0] != str[-1]:
+        return False
+    return is_palindrome(str[1:-1])
 
 # digit_match
 
-
+def digit_match(num1, num2, matches=0):
+    if num1 % 10 == num2 % 10:
+        matches += 1
+    if num1 // 10 and num2 // 10:
+        return digit_match(num1 // 10, num2 // 10, matches)
+    else:
+        return matches
