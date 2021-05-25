3a6d9f4010424c2a1053afca4c9c6cf5928faa95
Saejin Kwak Tanguay <saejinkwak@gmail.com>
2021-05-25 15:34:27 -0700

    saejin's answers for two recursion problem sets

--- part-1.py
+++ part-1.py
@@ -5,16 +5,52 @@
 
 # factorial
 
-
+def factorial(n):
+    if n == 0:
+        return 1
+    elif n > 0: 
+        return n * (factorial(n-1))
+    else:
+        raise ValueError
 
 # reverse
 
-
+def reverse(text):
+    
+    if len(text) >= 1:
+        #text[::-1] works buuuut
+        #base case is text[:1]- stop at index 1
+        #text [1:] -everything else
+        
+        first_letter = text[:1]
+        remaining_letters = text[1:]
+        return reverse(remaining_letters) + first_letter
+        
+    else: 
+        return ""
 
 # bunny
 
-
+def bunny(count):
+    if count == 0:
+        return 0
+    else:
+        return 2+ bunny(count-1)
 
 # is_nested_parens
 
+def is_nested_parens(parens, left_i=0):
+    right_i = len(parens) -1 -left_i
+    
+    if len(parens) == 0:
+        return True
+        
+    if left_i >= right_i:
+        return True
+   
+    if parens[left_i] == "(" and parens[right_i] == ")":
+        return is_nested_parens(parens, left_i+1)
+    else: 
+        return False 
+        
 
--- part-2.py
+++ part-2.py
@@ -5,12 +5,39 @@
 
 # search
 
-
+def search(array, query, i=0):
+    if not array:
+        return False
+    elif array[i] == query:
+        return True
+    elif i == len(array)-1:
+        return False
+    else:
+        return search(array, query, i+1)
 
 # is_palindrome
 
-
+def is_palindrome(text, left_i = 0):
+    right_i = len(text) -1 -left_i
+    if not text:
+        return False
+    elif left_i >= right_i:
+        return True
+    elif text[left_i] == text[right_i]:
+        return is_palindrome(text, left_i + 1)
 
 # digit_match
 
+def digit_match(num1, num2):
+    if num1 == 0 and num2 ==0:
+        return 1
+    elif num1 < 10 or num2 < 10:
+        if num1%10 == num2%10:
+            return 1
+        return 0
+    
+    if num1 % 10 == num2 % 10:
+        return 1+digit_match(num1//10, num2//10)
+
+    return digit_match(num1//10, num2//10)
 
