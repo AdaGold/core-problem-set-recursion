c8ca3a4d32cf9770bcf0322caee3aae6af45b84d
Drew Taylor <drewlynntaylor@gmail.com>
2021-05-25 17:36:14 -0700

    all problems added

--- part-1.py
+++ part-1.py
@@ -4,17 +4,40 @@
 # the appropriate comment.
 
 # factorial
-
+def factorial(num):
+    if num == 0:
+        return 1
+    elif num < 0:
+        raise ValueError(num)
+    return num*factorial(num-1)
 
 
 # reverse
-
+def reverse(text):
+    if len(text) <= 1:
+        return text
+    first_letter = text[:1]
+    remaining_word = text[1:]
+    return reverse(remaining_word) + first_letter
 
 
 # bunny
-
+def bunny(count):
+    if count == 0:
+        return 0
+    return 2 + bunny(count-1)
 
 
 # is_nested_parens
-
+def is_nested_parens(parens, left_side = 0):
+    right_side = len(parens) -1 -left_side
+    
+    if len(parens) == 0:
+        return True
+    if left_side >= right_side:
+        return True
+    if parens[left_side] == "(" and parens[right_side] == ")":
+        return is_nested_parens(parens, left_side + 1)
+    else:
+        return False
 
