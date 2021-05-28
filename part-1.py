a673a098c386b24a98efce354ae27cb37e2ef7ef
Libby L. Gerber <libby@libbygerber.com>
2021-05-28 16:08:35 -0700

    submiting for feedback

--- part-1.py
+++ part-1.py
@@ -4,17 +4,42 @@
 # the appropriate comment.
 
 # factorial
+def factorial(n):
+    if n < 0:
+        raise ValueError
+    if n <= 1: 
+        return 1
 
+    return n * factorial(n - 1)
 
 
 # reverse
+def reverse(string):
 
+    if len(string) <= 0:
+        return string
+    else:
+        return reverse(string[1:]) + string[0]
 
 
 # bunny
-
+def bunny(count):
+    if count == 0:
+        return count
+  
+    return 2 + bunny(count - 1)
 
 
 # is_nested_parens
+def is_nested_parens(parens):
+    if parens == "":
+        return True
 
+    if len(parens) == 2 and parens[0] != parens[-1]:
+        return True
+  
+    if len(parens) == 1 or parens[0] == parens[-1]:
+        return False
+    
+    return is_nested_parens(parens[1:-1])
 
--- part-2.py
+++ part-2.py
@@ -4,13 +4,43 @@
 # the appropriate comment.
 
 # search
-
+def search(array, query):
+    if len(array) == 0:
+        return False
+    if array[0] == query:
+        return True
+    else:
+        return search(array[1:], query)
 
 
 # is_palindrome
+def is_palindrome(text):
+    if len(text) == 1 or len(text) == 0:
+        return True
 
+    if text[0] != text[-1]:
+        return False
+
+    return is_palindrome(text[1:-1])
 
 
 # digit_match
+def digit_match(int_1, int_2):
+    str_1 = str(int_1)
+    str_2 = str(int_2)
 
+    if str_1[-1] != str_2[-1]: 
+        str_1 = str_1[:-1]
+        str_2 = str_2[:-1]
+
+        return digit_match(str_1[:-1], str_2[:-1])
+  
+    else:
+        str_1 = str_1
+        str_2 = str_2
+        return digit_match(str_1[:-1], str_2[:-1])
+    
+    #I wasn't able to solve this one. There was a solution posted in slack, 
+    #which I didn't understand and would like to discuss at my next 1:1 if 
+    #possible
 
