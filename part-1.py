f5a4238bb7a925a823c1f0cb842313efbfbba107
weishanyang <weishanyang33@gmail.com>
2021-05-28 17:51:59 -0700

    upload solution for part1 and part2.

--- part-1.py
+++ part-1.py
@@ -4,17 +4,37 @@
 # the appropriate comment.
 
 # factorial
-
-
+def factorial(n):
+    if n < 0:
+        raise ValueError
+    if 0 <= n <= 1:
+        return 1
+    return n * factorial(n - 1)
 
 # reverse
-
-
+def reverse(text):
+    if len(text) == 0:
+        return text
+    return reverse(text[1:]) + text[0]
 
 # bunny
-
-
+def bunny(count):
+    if count == 0:
+        return 0
+    if count == 1:
+        return 2
+    return bunny(1) + bunny(count - 1)
 
 # is_nested_parens
-
+def is_nested_parens(parens):
+    a = ['()', '{}', '[]']
+    if len(parens) == 0:
+        return True
+    elif not any([True for i in a if i in parens]):
+        return False
+    else:
+        for i in a:
+            if i in parens:
+                parens = ''.join(parens.split(i))
+        return is_nested_parens(parens)
 
--- part-2.py
+++ part-2.py
@@ -4,13 +4,32 @@
 # the appropriate comment.
 
 # search
-
-
+def search(array, query):
+    if len(array) == 0:
+       return False
+    if len(array) == 1 and array[0] != query:
+        return False
+    if array[0] == query:
+        return True
+    return search(array[1:],query)
 
 # is_palindrome
-
-
+def is_palindrome(text):
+    if len(text) == 1 or not text:
+        return True
+    if text[0] != text[-1]:
+        return False
+    return is_palindrome(text[1:-1])
 
 # digit_match
-
+def digit_match(apples, oranges, count = 0):
+    if apples == 0 and oranges == 0:
+        return 1
+    if apples < 10 or oranges < 10:
+        if apples % 10 == oranges % 10:
+            count += 1
+        return count
+    if apples % 10 == oranges % 10:
+        count += 1
+    return digit_match(apples//10, oranges//10, count) 
 
