be06f802c54c6647c6f436932730864d06015f64
Priscille Gwet <ngomakak@gmail.com>
2021-05-28 17:54:20 -0700

    Added my solutions from Problem Set: Recursion Pt. 1 in  part-1.py
    Added my solutions from Problem Set: Recursion Pt. 2 in  part-2.py

--- part-1.py
+++ part-1.py
@@ -5,16 +5,46 @@
 
 # factorial
 
+def factorial(n):
+    
+    if n < 0:
+        raise ValueError
+        
+    if n == 0:
+        return 1
+    
+    return n * factorial(n-1)
 
 
 # reverse
 
+def reverse(text):
+
+    if len(text) <= 1:
+        return text
+
+    return text[-1] + reverse(text[:-1])
 
 
 # bunny
 
+def bunny(count):
+    
+    if count == 0:
+        return 0
+        
+    return 2 + bunny(count-1)
 
 
 # is_nested_parens
 
-
+def is_nested_parens(parens):
+    first_char = "("
+    last_char = ")"
+    
+    if len(parens) < 1:
+        return True
+    elif parens[0] != first_char or parens[-1] != last_char:
+        return False
+    
+    return is_nested_parens(parens[1:-1])
