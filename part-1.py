36bc8ce34f2690313d130a1c085623af81750279
Heidi Zhang <qingyuanhzhang@gmail.com>
2021-05-30 16:20:02 -0700

    completed part-1 and part-2, passed all tests

--- part-1.py
+++ part-1.py
@@ -4,17 +4,31 @@
 # the appropriate comment.
 
 # factorial
-
-
+def factorial(num): 
+    if num < 0: 
+        raise ValueError("Number must be greater than zero")
+    elif num <= 1: 
+        return 1 
+    return num * factorial(num -1)
 
 # reverse
-
-
+def reverse(text):
+    if len(text) <= 1: 
+        return text
+    return reverse(text[1:]) + text[0]
 
 # bunny
-
-
+def bunny(count): 
+    if count < 1: 
+        return 0 
+    return 2 + bunny(count -1)
 
 # is_nested_parens
-
+def is_nested_parens(parens): 
+    if len(parens) == 0: 
+        return True 
+    elif parens[0] + parens[-1] == "()":
+        return is_nested_parens(parens[1:-1])
+    else: 
+        return False
 
--- part-2.py
+++ part-2.py
@@ -4,13 +4,33 @@
 # the appropriate comment.
 
 # search
-
-
+def search(list, number): 
+    if len(list) == 0: 
+        return False 
+    elif list[0] == number: 
+        return True 
+    return search(list[1:], number)
 
 # is_palindrome
+def reverse(text):
+    if len(text) <= 1: 
+        return text
+    return reverse(text[1:]) + text[0]
 
-
+def is_palindrome(text): 
+    if reverse(text) == text: 
+        return True
+    return False
 
 # digit_match
+def digit_match(num_1, num_2): 
+    str_1 = str(num_1)
+    str_2 = str(num_2)
+    count = 0 
 
+    if len(str_1) == 0 or len(str_2) == 0: 
+        return count 
+    elif str_1[-1] == str_2[-1]: 
+        count += 1 
+    return count + digit_match(str_1[:-1], str_2[:-1])
 
