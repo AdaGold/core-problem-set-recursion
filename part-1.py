9ca5727d3561747a99789bd6c5f737d46fa3a734
Rock - Viri Vazquez <viridiana.vzq@gmail.com>
2021-05-31 14:22:17 -0700

    Recursion problem solutions

--- part-1.py
+++ part-1.py
@@ -4,17 +4,36 @@
 # the appropriate comment.
 
 # factorial
-
+def factorial(n):
+    if n<0:
+        raise ValueError
+    if n <= 1:
+        return 1
+    return n * factorial(n-1)
 
 
 # reverse
-
+def reverse(in_str):
+    if in_str == "":
+        return in_str
+    
+    first_char = in_str[:1]
+    remaining_chars = in_str[1:]
+    return reverse(remaining_chars) + first_char
 
 
 # bunny
-
+def bunny(count):
+    if count <= 0:
+        return 0
+    return 2 + bunny(count-1)
 
 
 # is_nested_parens
-
+def is_nested_parens(parens):
+    if parens == "":
+        return True
+    elif parens[0] != "(" or parens[-1] != ")":
+        return False
+    return is_nested_parens(parens[1:-1])
 
--- part-2.py
+++ part-2.py
@@ -4,13 +4,33 @@
 # the appropriate comment.
 
 # search
-
+def search(in_array, query):
+    if len(in_array) <= 0:
+        return False
+    elif in_array[0] == query:
+        return True
+    new_array = in_array[1:]
+    return search(new_array, query)
 
 
 # is_palindrome
-
+def is_palindrome(text):
+    if len(text) <= 1:
+        return True
+    elif text[0] == text[-1]:
+        return is_palindrome(text[1:-1])
+    else:
+        return False
 
 
 # digit_match
-
+def digit_match(apples, oranges):
+    if apples <= 1 or oranges <= 1:
+        if apples != oranges:
+            return 0
+        else: 
+            return 1
+    else:
+        matching_pairs = apples % 10 == oranges % 10
+        return matching_pairs + digit_match(apples//10, oranges//10)
 
