f62ac8c855ede137c953c2b8dae3a95b9358fe9a
KARLA TORRES <t.karla212@gmail.com>
2021-06-01 11:46:30 -0400

    Entered answers for recursion problem set

--- part-2.py
+++ part-2.py
@@ -5,12 +5,44 @@
 
 # search
 
+def search(array, query):
+    # base case: if empty then search return False
+    if len(array) == 0:
+        return False
+    
+    if array[0] == query:
+         return True
+    else :
+        return search(array[1:], query)
 
 
 # is_palindrome
 
+def is_palindrome(text):
+    #Base case
+    if len(text) == 0: 
+        return True
+        
+    if text[0] != text[-1]:
+        return False
+        
+    return is_palindrome(text[1:-1])
+        
+        
 
 
 # digit_match
 
+def digit_match(x,y):
+    #Basecase
+    if x//10 == 0 or y//10 == 0:
+        if x%10 == y%10:
+            return 1
+        else:
+            return 0
 
+        
+    if x % 10 == y % 10:
+        return 1 + digit_match(x//10, y//10)
+    else:
+        return digit_match(x//10, y//10)
