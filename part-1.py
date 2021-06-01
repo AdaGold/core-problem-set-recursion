df55ee9eaf8c02cbe837b02fe7276474fd372d54
Aida <aida.rysbek@gmail.com>
2021-05-31 23:50:33 -0700

    Solutions for problem set 1and2

--- part-1.py
+++ part-1.py
@@ -4,17 +4,37 @@
 # the appropriate comment.
 
 # factorial
-
+def factorial(n):
+    if n == 0:
+        return 1
+    elif n > 0:
+        return n * factorial(n-1)
+    
+    raise ValueError
 
 
 # reverse
+def reverse(text):
+    if len(text) <= 1:
+        return text
 
+    return reverse(text[1:]) + text[:1]
 
 
 # bunny
-
+def bunny(count):
+    if count == 0:
+        return count
+        
+    return bunny(count-1) + 2
 
 
 # is_nested_parens
-
+def is_nested_parens(parens):
+    if len(parens) == 0:
+        return True
+    elif parens[:1] == "(" or parens[-1:] != ")":
+        return is_nested_parens(parens[1:-1])
+    else:
+        return False
 
--- part-2.py
+++ part-2.py
@@ -4,13 +4,38 @@
 # the appropriate comment.
 
 # search
-
+def search(array, query):
+    if len(array) == 0:
+        return False
+    elif array[0] != query:
+        return search(array[1:], query)
+    
+    return True
 
 
 # is_palindrome
-
+def is_palindrome(text):
+    if len(text) < 2:
+        return True
+    
+    if text[:1] == text[-1:]:
+        return is_palindrome(text[1:-1])
+    
+    return False
 
 
 # digit_match
+def digit_match(apples, oranges):
+    if apples == 0 and oranges == 0:
+        return 1 
 
+    elif apples < 10 or oranges < 10: 
+        if apples % 10 == oranges % 10: 
+            return 1
+        return 0
+
+    if apples % 10 == oranges % 10: 
+            return 1 + digit_match(apples // 10, oranges // 10) 
+
+    return  digit_match(apples // 10, oranges // 10)
 
