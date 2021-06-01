8d4a15fad5ad905b9d31466203cdc3f37a34a4a8
Leilani Allen <leilani.allen90@gmail.com>
2021-06-01 00:06:12 -0700

    All tests passing in part 1 and part 2

--- part-1.py
+++ part-1.py
@@ -4,17 +4,36 @@
 # the appropriate comment.
 
 # factorial
-
+def factorial(n):
+    if n < 0:
+        raise ValueError('n cannot be negative number')
+    
+    if n == 0:
+        return 1
+    return factorial(n-1) * n
+    
 
 
 # reverse
-
+def reverse(text):
+    text = text[::-1]
+    return text
 
 
 # bunny
-
+def bunny(count):
+    if count == 0:
+        return 0
+        
+    return bunny(count-1) + 2  
 
 
 # is_nested_parens
-
+def is_nested_parens(parens):
+    if len(parens) == 0:
+        return True
+    elif parens[:1] != "(" or parens[-1:] != ")":
+        return False
+    else:
+        return is_nested_parens(parens[1:-1])  
 
--- part-2.py
+++ part-2.py
@@ -4,13 +4,34 @@
 # the appropriate comment.
 
 # search
-
+def search(array, query):
+    if len(array) == 0:
+        return False
+    elif query == array[-1]:
+        return True
+    return search(array[:-1], query)
+    
 
 
 # is_palindrome
-
+def is_palindrome(text):
+    if len(text) <= 1:
+        return True
+    elif text[:1] != text[-1:]:
+        return False
+    else:
+        return is_palindrome(text[1:-1])
 
 
 # digit_match
+def digit_match(num1, num2): 
+    str1 = str(num1)
+    str2 = str(num2)
+    count = 0 
 
+    if len(str1) == 0 or len(str2) == 0: 
+        return count 
+    elif str1[-1] == str2[-1]: 
+        count += 1 
+    return count + digit_match(str1[:-1], str2[:-1])
 
