6b051fb0dcccf7b569f455ea1ac65e1efc050c5f
Ivana <ivanacramos@gmail.com>
2021-06-01 08:57:33 -0700

    Problem Sets!

--- part-1.py
+++ part-1.py
@@ -1,20 +1,41 @@
-# There are comments with the names of
-# the required functions to build.
-# Please paste your solution underneath
-# the appropriate comment.
+# Part 1
 
-# factorial
+# factorial❗
+def factorial(num):
+    if num < 0:
+        raise ValueError
+    if num == 0:
+        return 1
+    return num * factorial(num -1)
 
+# 〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️
 
+# reverse 🔄
+def reverse(text):
+    if len(text) == 0:
+        return ""
+    return text[-1] + reverse(text[:-1])
 
-# reverse
+# 〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️
 
+# bunny 🐰
+def bunny(count):
+    if count <= 0:
+        return 0
+    return 2 + bunny(count -1)
 
+# 〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️
 
-# bunny
+# is_nested_parens? 🤰
+def nested_helper(parens, counter=0): # 🦄 helper
+    if len(parens) == 0:
+        return True if counter == 0 else False
 
+    counter = counter + 1 if parens[0] == "(" else counter -1
 
+    return counter >= 0 and nested_helper(parens[1:], counter)
 
-# is_nested_parens
+def is_nested_parens(parens): # 🤰
+    return nested_helper(parens, 0)
 
-
+print(is_nested_parens("()(()()((()()))()())()"))
--- part-2.py
+++ part-2.py
@@ -1,16 +1,59 @@
-# There are comments with the names of
-# the required functions to build.
-# Please paste your solution underneath
-# the appropriate comment.
-
-# search
+# Part 2
 
 
+# search 🔍
+def search(array, query):
+    if len(array) == 0:
+        return False
+    if array[-1] == query:
+        return True
+    return search(array[:-1:], query)
 
-# is_palindrome
+# 〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️
+
+# is_palindrome? 👯‍♀️
+def is_palindrome(text):
+    if len(text) <= 1:
+        return True
+    if text[0] == text[-1]:
+        return is_palindrome(text[1:-1])
+    else:
+        return False
+print(is_palindrome("rats live on no evil star"))
+
+# or also 👯‍♀️💡:
+def is_palindrome_no_slice(text, first, last): # helper 🦄
+    if (first >= last):
+        return True
+    
+    if (text[first] == text[last]):
+        return is_palindrome_no_slice(text, first+1, last-1)
+    else:
+        return False
+
+def is_palindrome_two(text):
+    return is_palindrome_no_slice(text, 0, len(text)-1)
+
+print(is_palindrome_two("rats live on no evil star"))
+
+# 〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️
+
+# digit_match ✅
+def digit_match(num1, num2):
+    if (num1 == 0 and num2 == 0):
+        return 1
+    else:
+        return digi_matcher(num1, num2)
 
 
+def digi_matcher(num1, num2): # helper 🦄
+    if (num1 == 0 or num2 == 0):
+        return 0
 
-# digit_match
+    num1_mod = num1 % 10
+    num2_mod = num2 % 10
 
+    if num1_mod == num2_mod:          
+        return 1 + digi_matcher(num1//10, num2//10)
 
+    return digi_matcher(num1//10, num2//10)
