5e889598ef570efc47b65ce41b5ee1fbb03d68f3
Lacy Draper <ldrape1@gmail.com>
2021-05-31 21:03:48 -0700

    pasted recursion solutions

--- part-1.py
+++ part-1.py
@@ -4,17 +4,51 @@
 # the appropriate comment.
 
 # factorial
-
-
+def factorial(num):
+    if num < 0:
+        raise ValueError
+    
+    
+    if num == 0:#base case
+        return 1
+    return num * factorial(num-1)
+    # (4)
+      #4 * (4-1)
+        #3 * (3-1)
+         #2 * (2-1)
+          #1 
 
 # reverse
-
+def reverse(text):
+    if text =="":
+        return text
+    return text[-1] + reverse(text[:-1])# [:-1] makes is shorter
+#  "lacy"   
+#     "y" + reverse("lac")
+#      "y" + "c" + reverse("la")
+#       "y" + "c" + "a" + reverse("l")
+#         "y" +"c" + "a" + "l"  + reverse("")
+#           y + c + a + l
 
 
 # bunny
-
+def bunny(count):
+    
+    if count == 0:
+        return 0
+    return  2  + bunny(count - 1)
+    
+    #bunny(1)= 2 +bunny(0)= 0 
 
 
 # is_nested_parens
 
-
+def is_nested_parens(parens):
+    
+    if parens == "": #base case
+        return True
+    if parens[0] =='(' and parens[-1] == ')':
+        return True and is_nested_parens(parens[1:-1])
+    return False
+    #result = parens[0] == parens[-1]#result holds a boolean value
+   
--- part-2.py
+++ part-2.py
@@ -4,13 +4,41 @@
 # the appropriate comment.
 
 # search
+def search(array,query):
+    if array == []:
+        return False
+    result = array[-1] == query
+    if result:
+        return True
+    return search(array[:-1],query)
+    
 
 
 
 # is_palindrome
-
+def is_palindrome(text):
+    if not text:
+        return True
+    if len(text)==1:
+        return True 
+    result = text[0]==text[-1]
+    if result == False:
+        return False
+    return result and is_palindrome(text[1:-1])# decreasing last and first element each time
+    
 
 
 # digit_match
-
-
+#i cant spot where i am going wrong here
+def digit_match(num1,num2):
+    count = 0
+    if num1 == 0 and num2 == 0:
+        return 1
+    if num1 == 0 or num2 == 0:
+        return 0
+    
+    result = num1 % 10 == num2 % 10
+    if result:
+        count = 1
+    
+    return count + digit_match(num1 // 10, num2 // 10)
