80d79869aaf24ebf191831b3ebc1f9ac0182049b
Jittania <jittania@gmail.com>
2021-06-01 00:28:12 -0700

    pt 1 done

--- part-1.py
+++ part-1.py
@@ -5,16 +5,41 @@
 
 # factorial
 
+def factorial(n):
+    if n == 0:
+        return 1
+    try:
+        return factorial(n-1) * n 
+    except RecursionError:
+        raise ValueError
 
-
+#=============================================================================
 # reverse
 
-
-
+def reverse(text):
+    if len(text) == 0:
+        return text
+    else:
+        # text[1:] slices the string except for the first character 
+        return reverse(text[1:]) + text[0]
+    
+#=============================================================================
 # bunny
 
+def bunny(count):
+    if count == 0:
+        return 0
+    else:
+        return 2 + bunny (count - 1)
 
-
+#=============================================================================
 # is_nested_parens
 
-
+def is_nested_parens(parens, i=0, j=0):
+    if i == len(parens):
+        return j == 0
+    
+    if parens[i] == "(":
+        return is_nested_parens(parens, i+1, j+1)
+    elif parens[i] == ")":
+        return is_nested_parens(parens, i+1, j-1)
