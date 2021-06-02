0cc16c96bf216131308b95de6230adbaae0a379a
Marisa <marisa.n.morales@gmail.com>
2021-06-01 17:52:49 -0700

    recursion problem sets 1 + 2. Unsure about part 2 digit_match

--- part-1.py
+++ part-1.py
@@ -4,17 +4,44 @@
 # the appropriate comment.
 
 # factorial
+def factorial(n):
+    if n <0:
+        raise ValueError
+    elif n == 0:
+        return 1
 
+    return n * factorial(n-1)
 
 
 # reverse
+def reverse(text):
+    if text == "":
+        return ""
+    
+    return reverse(text[1:])+ text[0]
 
 
 
 # bunny
+def bunny(count):
+    if not count:
+        return count
+    else:
+        return 2 + bunny(count-1)
 
 
 
 # is_nested_parens
+def is_nested_parens(parens):
+    open_parens = "("
+    close_parens = ")"
+    
+    if len(parens) == 0:
+        return True
+    
+    elif parens[0]!= open_parens or parens[-1]!= close_parens:
+        return False
+    
+    return is_nested_parens(parens[1:-1])
 
 
