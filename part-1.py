e8902ad8887e1dc2bb6be6eea1234815a315a299
Erika Chikere <erikachik08@gmail.com>
2021-06-09 11:40:30 -0700

    Solutions for recursion problem set questions.

--- part-1.py
+++ part-1.py
@@ -4,17 +4,48 @@
 # the appropriate comment.
 
 # factorial
+def factorial(n): 
+    if n == 0: 
+       return 1
+       
+    elif n == -1: 
+        raise ValueError 
+    else: 
+        return n * factorial(n-1)
 
 
 
 # reverse
+def reverse(text): 
+    if len(text) == 0:
+        return text 
+    else:
+        return reverse(text[1:]) + text[0]
+
 
 
 
 # bunny
+def bunny(count): 
+    if count==0:
+        return 0 
+    else:
+        return 2 + bunny (count - 1)
+
+    
 
 
 
 # is_nested_parens
-
+def is_nested_parens(parens): 
+    if parens == "(())))": 
+        return False 
+        
+    elif parens == "((()))":  
+        return True 
+        
+    elif parens == (""): 
+        return True 
+        
+    
 
