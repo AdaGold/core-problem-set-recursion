ec3df5d817ae513ecfadb66782e63c431f069173
Lux Barker <lux.barker13@gmail.com>
2021-12-08 14:29:46 -0800

    add my solutions to part 1

--- part-1.py
+++ part-1.py
@@ -4,17 +4,36 @@
 # the appropriate comment.
 
 # factorial
-
+def factorial(n):
+    if n < 0:
+        raise ValueError
+    if n == 0:
+        return 1
+    return n * factorial(n-1)
 
 
 # reverse
-
+def reverse(text):
+    if len(text) == 0:
+        return ""
+    return reverse(text[1:]) + text[0]
 
 
 # bunny
-
+def bunny(count):
+    if count < 0:
+        raise ValueError("you can't have negative bunnies!")
+    if count == 0:
+        return 0
+    return bunny(count-1) + 2
 
 
 # is_nested_parens
+def is_nested_parens(parens):
+    if parens == "":
+        return True
+    if parens[0] == "(" and parens[-1] == ")":
+        return is_nested_parens(parens[1:-1])
+    return False
 
 
