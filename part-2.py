9b827501a35d600160ba8e14526ba0f10cb61e8f
Alma <abecerrilsalas@gmail.com>
2021-12-08 15:51:30 -0800

    part2

--- part-2.py
+++ part-2.py
@@ -4,13 +4,38 @@
 # the appropriate comment.
 
 # search
-
+def search (array, query):
+    if not len(array):
+        return False
+    elif array[0]==query:
+        return True
+    return search (array[1:], query)
 
 
 # is_palindrome
+def is_palindrome(text): 
+    if len(text) <= 1: 
+        return True 
 
+    if text[0] != text[-1]: 
+        return False 
+
+    return is_palindrome(text[1:-1])
 
 
 # digit_match
+def digit_match(digit1, digit2): 
+    first = str(digit1)
+    second = str(digit2)
+    match_count = 0 
+    
+    if len(first) == 0 or len(second) == 0: 
+        return match_count
 
+    if first[-1] == second[-1]: 
+        match_count += 1
+    else: 
+        match_count += 0
+    
+    return match_count + digit_match(first[:-1], second[:-1])
 
