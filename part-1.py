3b7e911a54f23439978a5c26de114c988e84ca8b
jacy <jacyyang04@gmail.com>
2021-12-09 08:37:52 -0800

    add reverse function

--- part-1.py
+++ part-1.py
@@ -4,15 +4,28 @@
 # the appropriate comment.
 
 # factorial
-
+def factorial(n):
+    if n < 0:
+        raise ValueError
+    if n == 1 or n==0:
+        return 1
+    return n * factorial(n-1)
 
 
 # reverse
+def reverse(text):
+    if type(text) != str:
+        raise TypeError
+    if len(text) == 0:
+        return text
+    return reverse(text[1:]) + text[0]
 
-
+# print(reverse("cat"))
 
 # bunny
-
+# count = [1,2,3,4]
+# def bunny(count):
+#     pass
 
 
 # is_nested_parens
