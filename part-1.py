c10d9d33f9cb578fcf310e68beca00d2ddba33c0
Michelle Bodart <michelle.bodart@gmail.com>
2021-12-09 09:25:30 -0800

    recursion ROCKS

--- part-1.py
+++ part-1.py
@@ -4,17 +4,36 @@
 # the appropriate comment.
 
 # factorial
-
+def factorial(n):
+    if n < 0:
+        raise ValueError("input must be >= 0")
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
 
 
 # reverse
-
+def reverse(text):
+    if text == "":
+        return text
+    else:
+        return text[-1] + reverse(text[:-1])
 
 
 # bunny
-
+def bunny(count):
+    if count == 0:
+        return 0
+    else:
+        return 2 + bunny(count - 1)
 
 
 # is_nested_parens
-
+def is_nested_parens(parens):
+    if not parens:
+        return True
+    else:
+        outer = (parens[0] == "(" and parens[-1] == ")")
+        return (outer and is_nested_parens(parens[1:-1]))
 
--- part-2.py
+++ part-2.py
@@ -4,13 +4,27 @@
 # the appropriate comment.
 
 # search
-
+def search(array, query):
+    if not array:
+        return False
+    else:
+        return array[0] == query or search(array[1:], query)
 
 
 # is_palindrome
-
+def is_palindrome(text):
+	if len(text) <= 1:
+		return True
+	else:
+		return text[0] == text[-1] and is_palindrome(text[1:-1])
 
 
 # digit_match
-
+def digit_match(m,n):
+	m = str(m)
+	n = str(n)
+	if not m or not n:
+		return 0
+	else:
+		return (1 if m[-1] == n[-1] else 0) + digit_match(m[:-1],n[:-1])
 
