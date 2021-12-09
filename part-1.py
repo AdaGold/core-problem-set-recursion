fd6e9a6f29b66cdbbbe8d95ee4f2291bce862ca7
jacy <jacyyang04@gmail.com>
2021-12-09 13:46:03 -0800

    add all recursion functions to part-1.py

--- part-1.py
+++ part-1.py
@@ -14,8 +14,6 @@ def factorial(n):
 
 # reverse
 def reverse(text):
-    if type(text) != str:
-        raise TypeError
     if len(text) == 0:
         return text
     return reverse(text[1:]) + text[0]
@@ -26,17 +24,38 @@ def reverse(text):
 # bunny
 
 def bunny(count):
-    if type(count) != int:
-        raise TypeError
     if count == 0:
         return 0
     elif count == 1:
         return 2
-    return 2 + bunny(count-1)
+    else:
+        return 2 + bunny(count-1)
 
 # print(bunny(50))
 # output: 100
 
 # is_nested_parens
+def is_nested_parens(parens):
 
+    if parens == "":
+        return True
 
+    # Base Case    
+    if parens[0] == parens[-1]:
+        return False
+    else:
+        parens[0] == "(" and parens[-1] == ")"
+        parens = parens[1:-1]
+        return is_nested_parens(parens)
+
+parens = "((()))"
+print(is_nested_parens(parens))
+# output: True
+
+parens2 = ""
+print(is_nested_parens(parens2))
+# output: True
+
+no_match = "(())))"
+print(is_nested_parens(no_match))
+# output: False
