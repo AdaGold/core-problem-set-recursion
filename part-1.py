3317031642c7a2a5d3e2b90887ab7669a6510104
Marjan <marjan.abm@gmail.com>
2021-12-09 18:54:15 -0800

    recursion problem set 1 , 2

--- part-1.py
+++ part-1.py
@@ -4,17 +4,33 @@
 # the appropriate comment.
 
 # factorial
-
-
+def factorial(n):
+    if n < 0:
+        raise ValueError
+    if n == 0 :
+        return 1
+    return n * (factorial(n - 1))
 
 # reverse
-
-
+def reverse(text):
+    if len(text) <= 1:
+        return text
+    return text[-1] + reverse(text[:-1])
 
 # bunny
-
+def bunny (count):
+    if count == 0:
+        return 0
+    return 2 + bunny(count-1)
 
 
 # is_nested_parens
+def is_nested_parens(parens):
+    if len(parens) == 0 :
+        return True
+    elif len(parens) % 2 != 0 :
+        return False
+    elif parens[0] == "(" and parens[-1] == ")":
+        return is_nested_parens(parens[1:-1])
 
 
--- part-2.py
+++ part-2.py
@@ -4,13 +4,35 @@
 # the appropriate comment.
 
 # search
-
+def search (array, query):
+    if len(array) == 0:
+        return False
+    elif query == array[0]:
+        return True
+    return search(array[1:],query)
 
 
 # is_palindrome
-
+def is_palindrome(text):
+    if len(text) <= 0 :
+        return True
+    elif text[0] != text[-1]:
+        return False
+    elif text[0] == text[-1]:
+        return is_palindrome(text[1:-1])
 
 
 # digit_match
-
+def digit_match(apples, oranges):
+    count = 0
+    app_str = str(apples)
+    or_str = str(oranges)
+    
+    if len(app_str) == 0 or len(or_str) == 0 :
+        return count
+        
+    elif app_str[-1] == or_str[-1]:
+        count += 1
+        
+    return count + digit_match(app_str[:-1],or_str[:-1])
 
