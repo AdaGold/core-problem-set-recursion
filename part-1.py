75e2bfd35b88c9eca639ed237b0607de343aa10a
mac-madison <macanmadison@gmail.com>
2021-12-10 10:43:47 -0800

    answers 1 & 2

--- part-1.py
+++ part-1.py
@@ -4,17 +4,38 @@
 # the appropriate comment.
 
 # factorial
+def factorial(n):
+    if n < 0:
+        raise ValueError
+    elif n == 0:
+        return 1
 
+    else:
+        return n * factorial(n - 1)
 
 
 # reverse
-
+def reverse(text):
+    if len(text) == 0:
+        return text
+    return reverse(text[1:]) + text[0]
 
 
 # bunny
-
+def bunny(count):
+    if count == 0:
+        return count
+    else:
+        return bunny(count - 1) + 2
 
 
 # is_nested_parens
 
 
+def is_nested_parens(parens):
+    if len(parens) == 0:
+        return True
+    if parens[0] == "(" and parens[-1] == ")":
+        return is_nested_parens(parens[1:-1])
+
+    return False
--- part-2.py
+++ part-2.py
@@ -4,13 +4,42 @@
 # the appropriate comment.
 
 # search
+def search(array, query, i=0):
+    if len(array) == i:
+        return False
+    elif array[i] == query:
+        return True
 
+    else:
+        return search(array, query, i + 1)
 
 
 # is_palindrome
 
 
+def is_palindrome(text):
+    def helper(left, right):
+        if right - left <= 0:
+            return True
+        elif text[left] == text[right]:
+            return helper(left + 1, right - 1)
+        return False
+
+    return helper(0, len(text) - 1)
+
 
 # digit_match
 
 
+def digit_match(num1, num2):
+    if num1 == 0 and num2 == 0:
+        return 1
+
+    elif num1 < 10 or num2 < 10:
+        if num1 % 10 == num2 % 10:
+            return 1
+        return 0
+
+    if num1 % 10 == num2 % 10:
+        return 1 + digit_match(num1 // 10, num2 // 10)
+    return digit_match(num1 // 10, num2 // 10)
