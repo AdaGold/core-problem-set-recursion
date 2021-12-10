29e28498abc64c87d41c4f1f0b0b9964d4c72891
Mac <mackenziespeyton@gmail.com>
2021-12-09 18:46:23 -0800

    Adds code to part-1

--- part-1.py
+++ part-1.py
@@ -4,17 +4,35 @@
 # the appropriate comment.
 
 # factorial
-
-
+def factorial(n): 
+    if n < 0:
+        raise ValueError
+    elif n == 0: 
+        return 1
+    else: 
+        return n * factorial(n-1)
 
 # reverse
-
-
+def reverse(text): 
+    if len(text) == 0: 
+        return text
+    else: 
+        return reverse(text[1:]) + text[0]
 
 # bunny
-
-
+def bunny(count):
+    if count == 0:
+        return count
+    else:
+        return 2 + bunny(count-1)
 
 # is_nested_parens
-
+def is_nested_parens(parens): 
+    if not parens: 
+        return True
+    else:
+        if parens[0] == "(" and parens[-1] == ")": 
+            return is_nested_parens(parens[1:-1])
+        else: 
+            return False
 
