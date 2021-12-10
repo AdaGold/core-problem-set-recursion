f8e806e31d702da6a17ec5912b694e8af970f8e7
Lizet Tovar <lizett503@gmail.com>
2021-12-10 11:20:02 -0800

    Problem Set 1 & 2

--- part-1.py
+++ part-1.py
@@ -4,17 +4,33 @@
 # the appropriate comment.
 
 # factorial
-
-
+def factorial(n):
+    if n<0:
+        raise ValueError("We can't do negative numbers!")
+    elif n==0:
+        return 1
+    else:
+        return factorial (n-1) * n
 
 # reverse
-
-
+def reverse (text):
+    if text == "":
+        return text
+    return reverse(text[1:]) + text[0]
 
 # bunny
-
-
+def bunny(count):
+    # base case
+    if count == 0:
+        return count
+    return 2 + bunny(count-1)
 
 # is_nested_parens
-
+def is_nested_parens(parens):
+    if parens == "":
+        return True
+    if parens[0] == "(" and parens[-1] == ")":
+        return is_nested_parens(parens[1:-1])
+    else:
+        return False
 
--- part-2.py
+++ part-2.py
@@ -4,13 +4,40 @@
 # the appropriate comment.
 
 # search
-
+def search(array, query, index=0):
+    # two base cases
+    if len(array) == index:
+        return False
+    elif array[index] == query:
+        return True
+    else:
+        return search(array,query, index+1)
 
 
 # is_palindrome
-
+def is_palindrome(text):
+    def helper(left, right):
+        if right - left <= 0:
+            return True
+        elif text[left]==text[right]:
+            return helper(left +1, right -1)
+        else:
+            return False
+    return helper(0, len(text) -1)  
 
 
 # digit_match
-
-
+def digit_match(left, right, matches=0):
+    if left == 0 and right ==0:
+        return matches + 1
+    elif left < 10 or right <10:
+        if left % 10 == right % 10:
+            return matches +1
+        else:
+            return matches
+    elif left %10 == right%10:
+        return digit_match(left//10, right//10, matches +1)
+    else:
+        return digit_match(left//10, right//10, matches)
+    
+    
