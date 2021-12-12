9ba0d215c091de087c18cad4f3d17926fae6b893
Areeba Rashid <areeba.r.qureshi@gmail.com>
2021-12-11 22:56:27 -0800

    problem sets pasted

--- part-1.py
+++ part-1.py
@@ -4,17 +4,71 @@
 # the appropriate comment.
 
 # factorial
+def factorial(n):
+    if n < 0: 
+        raise ValueError("no negatives allowed!")
+    
+    if n == 0:
+        return 1
+    return n * factorial(n-1)
 
 
 
 # reverse
 
-
+def reverse(text):
+    if text == "":
+        return text
+    else:
+        print(text[1:])
+        return reverse(text[1:]) + text[0]
 
 # bunny
-
+def bunny(count):
+    if count == 0:
+        return count 
+    
+    return bunny(count-1) + 2
+    
+    #bunny(count = 0)
+    #count = 0
+    #return 0
+    #--------
+    #bunny(count = 1)
+    #count = 1
+    #0 + 2
+    #-------
+    #bunny(count = 2)
+    #count = 2
+    #2 + 2
+    #-------
+    #bunny(count = 3)
+    #count = 3
+    #4 + 2
+    
+    #_______________________________________
+    #0 bunnies => 0
+    #3 bunnies => 6
 
 
 # is_nested_parens
 
-
+def is_nested_parens(parens):
+    if parens == "":
+        return True
+        
+    else:
+        
+        s1 = parens[:len(parens)//2]
+        s2 = parens[len(parens)//2:]
+        #print(s1,s2)
+        
+        for paren in s1:
+            if paren != "(":
+                return False
+        
+        for paren in s2:
+            if paren != ")":
+                return False
+        
+        return True
--- part-2.py
+++ part-2.py
@@ -4,13 +4,33 @@
 # the appropriate comment.
 
 # search
-
+def search(array, query):
+    if not len(array):
+        return False
+    elif array[0] == query:
+        return True
+    return search(array[1:], query)
 
 
 # is_palindrome
-
+def is_palindrome(word):
+    if len(word) == 1:
+        return True
+    
+    if word[0] != word[-1]: return False
+    
+    return is_palindrome(word[1:-1])
 
 
 # digit_match
-
+def digit_match(a, b): 
+    if a == 0 and b == 0: 
+        return 1
+    else:
+        result = 0
+        if (a % 10) == (b % 10):
+            result+=1
+        if (a//10 == 0) or (b//10 == 0):
+            return result
+        return result + digit_match(a//10, b//10)
 
