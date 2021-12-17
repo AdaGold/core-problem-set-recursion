19c329d6f0c44f31fde45620592ffcadc8fb593c
Alie Ibarra <alieibarra@gmail.com>
2021-12-17 10:13:51 -0800

    Add functions from recursion problem set 1 & 2

--- part-1.py
+++ part-1.py
@@ -4,17 +4,36 @@
 # the appropriate comment.
 
 # factorial
-
+def factorial (n):
+    if n < 0:
+        raise ValueError("Numbers greater than 0, please")
+    elif n == 0:
+        return 1
+    return n * factorial(n-1)
 
 
 # reverse
-
+def reverse (text, i=0):
+    if text == "":
+        return text
+    return reverse(text[1:])+ text[0]
 
 
 # bunny
-
+def bunny(count):
+    if count == 0:
+        return 0
+    return 2 + bunny(count - 1)
 
 
 # is_nested_parens
-
+def is_nested_parens(parens):
+    first = "("
+    last = ")"
+    if parens == "":
+        return True
+    if parens[0] == first and parens[-1] == last:
+        return is_nested_parens(parens[1:-1])
+    else:
+        return False
 
--- part-2.py
+++ part-2.py
@@ -4,13 +4,34 @@
 # the appropriate comment.
 
 # search
-
-
+def search(array, query):
+    if array == []:
+        return False
+    if array[0] == query:
+        return True
+    array.remove(array[0])
+    return search(array, query)
 
 # is_palindrome
-
-
+def is_palindrome(text):
+    if len(text) == 1:
+        return True
+    if text[0] != text [-1]:
+        return False
+    return is_palindrome(text[1:-1])
 
 # digit_match
-
+def digit_match(num1, num2):
+    if num1 == 0 and num2 == 0:
+        return 1
+    
+    elif num1 < 10 or num2 < 10:
+        if num1 % 10 == num2 % 10:
+            return 1
+        return 0
+    
+    if num1 % 10 == num2 % 10:
+        return 1 + digit_match(num1//10, num2//10)
+    
+    return digit_match(num1//10, num2//10)
 
