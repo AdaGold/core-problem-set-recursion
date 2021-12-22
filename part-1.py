c2b42032f91fe6c44a7adf10f8f964b6135b53d2
Sophia Le <sophiale@sophias-mbp.lan>
2021-12-21 21:50:11 -0800

    Adds part1 solutions

--- part-1.py
+++ part-1.py
@@ -4,17 +4,42 @@
 # the appropriate comment.
 
 # factorial
-
+def factorial(num):
+    if num < 0:
+        raise ValueError()
+        
+    if num == 0:
+        return 1
+        
+    return factorial(num - 1) * num
 
 
 # reverse
-
+def reverse(s):
+    if len(s) <= 1:
+        return s
+    
+    return s[-1] + reverse(s[:-1])
 
 
 # bunny
-
+def bunny(count):
+    if count < 0:
+        raise ValueError()
+    
+    if count == 0:
+        return 0
+    return bunny(count - 1) + 2
 
 
 # is_nested_parens
+def _is_nested_parens(n):
+    if not n:
+        return 0
+    
+    v = 1 if n[0] == '(' else -1
+    return v + _is_nested_parens(n[1:])
 
+def is_nested_parens(n):
+    return 0 == _is_nested_parens(n)
 
