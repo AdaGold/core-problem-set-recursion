d8080f3ed52672fcf57ec8525dc76184df5e1237
KitSutliff <38538497+KitSutliff@users.noreply.github.com>
2021-12-31 10:56:51 -0800

    Completed with help from students and class

--- part-1.py
+++ part-1.py
@@ -5,16 +5,42 @@
 
 # factorial
 
+def factorial(n):
+    if n < 0:
+        raise ValueError ("keep it positive")
+    if n == 0:
+        return 1
+    else: 
+        return factorial(n-1) * n
+
 
 
 # reverse
 
-
+def reverse(text):
+    if len(text) == 0:
+        return text
+    else:
+        return reverse(text[1:]) + text[0]
 
 # bunny
 
-
+def bunny(count):
+    if count == 0:
+        return 0
+    elif count ==1:
+        return 2
+    else:
+        return 2 + bunny(count-1)
 
 # is_nested_parens
+
+def is_nested_parens(parens):
+    if parens == "":
+        return True
+    if parens[0] == "(" and parens[-1] == ")":
+        return is_nested_parens(parens[1:-1])
+    else:
+        return False
 
 
--- part-2.py
+++ part-2.py
@@ -5,12 +5,39 @@
 
 # search
 
+def search(array, query, i=0):
+    if len(array) == i:
+        return False
+    elif array[i] == query:
+        return True
+    else:
+        return search(array, query, i + 1)
 
 
 # is_palindrome
 
+def is_palindrome(text):
+    if len(text) == 1:
+        return True
+    elif text[0] == text[-1]:
+        return is_palindrome(text[1:-1])
+    return False
 
 
 # digit_match
 
+def digit_match(left, right, matches=0):
+    if left == 0 and right == 0:
+        return matches + 1
 
+    if left < 10 or right < 10:
+        if left % 10 == right % 10:
+            return matches + 1
+        else:
+            return matches
+
+    elif left % 10 == right % 10:
+        return digit_match(left//10, right//10, matches + 1)
+
+    else:
+        return digit_match(left//10, right//10, matches)
