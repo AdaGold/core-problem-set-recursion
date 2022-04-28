767bbd0a415d3ba1d3957a991f53a84e2f37aa0a
Emily Schell <lauraemilyschell@gmail.com>
2022-04-27 20:29:52 -0400

    initial commit

--- part-1.py
+++ part-1.py
@@ -4,17 +4,33 @@
 # the appropriate comment.
 
 # factorial
-
+def factorial(n):
+    if n == 0:
+        return 1
+    if n < 0:
+        raise ValueError
+    return n * factorial(n-1)
 
 
 # reverse
-
+def reverse(text):
+    if len(text) <= 1:
+        return text
+    return text[len(text)-1] + reverse(text[:-1])
 
 
 # bunny
-
+def bunny(count):
+    if count == 0:
+        return 0
+    return 2 + bunny(count - 1)
 
 
 # is_nested_parens
-
-
+def is_nested_parens(parens):
+    if not parens:
+        return True
+    elif parens[0] == "(" and parens[-1] == ")" and is_nested_parens(parens[1:-1]):
+        return True
+    else:
+        return False
--- part-2.py
+++ part-2.py
@@ -4,13 +4,37 @@
 # the appropriate comment.
 
 # search
+def search(array, query):
 
+    if not all([array, query]):
+        return False
+
+    if array[-1] == query:
+        return True
+    else:
+        array.pop()
+        return search(array, query)
 
 
 # is_palindrome
-
+def is_palindrome(text):
+    if len(text) <= 1:
+        return True
+    elif text[0] == text[-1] and is_palindrome(text[1:-1]):
+        return True
+    else:
+        return False
 
 
 # digit_match
+def digit_match(int1, int2):
+    if int1 == 0 and int2 == 0:
+        return 1
 
-
+    else:
+        count = 0
+        if int1 % 10 == int2 % 10:
+            count += 1
+        if int1//10 > 0 and int2//10 > 0:
+            count += digit_match(int1//10, int2//10)
+    return count
