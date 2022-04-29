48c1e7ec5634b5e3ace1be8bdb8d482c14b57a1f
NinaPatrina <ninochca@gmail.com>
2022-04-28 21:17:58 -0700

    part--2 solutions added

--- part-2.py
+++ part-2.py
@@ -4,13 +4,43 @@
 # the appropriate comment.
 
 # search
-
-
-
+def search(array, query):
+    if not array:
+        return False
+    if array[0]==query:
+        return True
+    
+    return search(array[1:],query)
+    
 # is_palindrome
 
+def is_palindrome(text):
+    if len(text)<=1:
+        return True
+    
+    return text[0]==text[-1] and is_palindrome(text[1:-1])
+
+# is_palindrome_challenge
+# 
+def is_palindrome(text):
+    text=text.lower()
+    left=0
+    right=len(text)-1
+
+    return helper_function(text, left, right)
+
+def helper_function(text, left, right):    
+    if left>=right:
+        return True
+    
+    return text[left]==text[right] and helper_function(text, left+1, right-1)    
 
 
 # digit_match
 
+def digit_match(apples,oranges):
+    if apples<10 or oranges<10:
+        return int(apples%10==oranges%10)
 
+    return int(apples%10==oranges%10)+digit_match(apples//10,oranges//10)
+    
