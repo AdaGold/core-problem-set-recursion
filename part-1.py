98ea338268ad7da2f3635ce0b3a94b67df778c2e
Joanna Dudley <jfdudleybmc@gmail.com>
2022-04-30 18:38:27 -0400

    completed problem set parts 1 and 2

--- part-1.py
+++ part-1.py
@@ -4,17 +4,35 @@
 # the appropriate comment.
 
 # factorial
+def factorial(num):
+    if num < 0:
+        raise ValueError
+    elif num == 0:
+        return 1
+    return num * factorial(num-1)
 
 
 
 # reverse
-
+def reverse(word):
+    if len(word) <= 1:
+        return word
+    return word[-1] + reverse(word[:-1])
 
 
 # bunny
-
+def bunny(bunnies):
+    if bunnies == 0:
+        return 0
+    return 2 + bunny(bunnies-1)
 
 
 # is_nested_parens
-
+def is_nested_parens(string):
+    if not string:
+        return True
+    if f"{string[0]}{string[-1]}" == "()" and is_nested_parens(string[1:-1]):
+        return True
+    else:
+        return False
 
