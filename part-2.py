fbbc11d98c1068ee4cb893de1efd62a0156cda17
Roshni Patel <rpatel01@wesleyan.edu>
2022-05-07 16:30:15 -0700

    Complete is_palindrome and search

--- part-2.py
+++ part-2.py
@@ -4,13 +4,26 @@
 # the appropriate comment.
 
 # search
-
+def search(array, query):
+    if len(array) == 0:
+        return False
+    elif array[0] == query:
+        return True
+    else:
+        return search(array[1:], query)
 
 
 # is_palindrome
-
+def is_palindrome(text):
+    if len(text) == 1:
+        return True
+    elif text[0] == text[-1]:
+        return is_palindrome(text[1:-1])
+    else:
+        return False 
+        
 
 
 # digit_match
-
-
+def digit_match(n1, n2):
+    
