b0e0ff9643f88706232e5de5fa5715b8f65da9b3
Kiana <kianashui@gmail.com>
2022-05-07 20:54:22 -0700

    added part-1 and part-2 functions

--- part-1.py
+++ part-1.py
@@ -4,17 +4,45 @@
 # the appropriate comment.
 
 # factorial
-
-
+def factorial(n):
+    if n < 0:
+        raise ValueError
+    if n == 0:
+        return 1
+    return n * factorial(n-1)
 
 # reverse
-
+def reverse(text):
+    if text == "":
+        return ""
+    try:
+        reversed_text += text[-1]
+    except UnboundLocalError:
+        reversed_text = text[-1]
+    
+    return reversed_text + reverse(text[:-1])
 
 
 # bunny
+def bunny(count):
+    if count < 0:
+        raise ValueError
+    elif count == 0:
+        return 0
+    elif count == 1:
+        return 2
+    return 2 + bunny(count-1)
 
 
 
 # is_nested_parens
-
-
+def is_nested_parens(parens):
+    if parens == "":
+        return True
+    if len(parens) % 2 != 0:
+        return False
+    if parens[0] == "(" and parens[-1] == ")":
+        new_string = parens[1:-1]
+    else:
+        return False
+    return is_nested_parens(new_string)
