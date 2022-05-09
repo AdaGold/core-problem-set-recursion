a5c52ffef8b6c50b7a9f2e5e508ace31d0e8eb84
Elaine Smith <oneile1219@gmail.com>
2022-05-08 22:46:42 -0700

    Recursion pt.1 & pt. 2 solutions

--- part-2.py
+++ part-2.py
@@ -4,13 +4,32 @@
 # the appropriate comment.
 
 # search
+def search(array, query):
+    if not array:
+        return False
+    if query == array[0]:
+        return True
 
+    return search(array[1:], query)
 
 
 # is_palindrome
-
+def is_palindrome(text):
+    if len(text) == 0:
+        return True
+    if text[0] != text[-1]:
+        return False
+    return is_palindrome(text[1:-1])
 
 
 # digit_match
-
+def digit_match(int1, int2):
+    match = 0
+    if int1 == "" or int2 == "":
+        return match
+        
+    if str(int1)[-1] == str(int2)[-1]:
+        match +=1
+    
+    return match + digit_match(str(int1)[:-1], str(int2)[:-1])
 
