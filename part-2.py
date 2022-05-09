56fd858435f6f9dc60c625609e596bc4e0a7e3d2
Taingleap Chheang <chheangtaingleap@gmail.com>
2022-05-09 16:11:26 -0700

    part1 & part2 solutions

--- part-2.py
+++ part-2.py
@@ -4,13 +4,38 @@
 # the appropriate comment.
 
 # search
+def search(array,query):
+    if not len(array):
+        return False
 
+    remaining_element = array[1:]
+    if query == array[0]:
+        return True
+    else:
+        return search(remaining_element,query)
 
 
 # is_palindrome
-
+def is_palindrome(text):
+    if len(text) <= 1:
+        return True
+    
+    if text[0] != text[len(text) -1]:
+        return False
+    else:
+        return is_palindrome(text[1:len(text)-1])
 
 
 # digit_match
-
+def digit_match(apples,oranges):
+    if apples < 10 or oranges < 10:
+        if apples % 10 == oranges % 10:
+            return 1
+        else:
+            return 0
+            
+    if apples % 10 == oranges % 10:
+        return 1 + digit_match(apples//10,oranges//10 )
+    else:
+        return digit_match(apples//10,oranges//10 )
 
