def8377311c20461cf6e16dd06ccadf07298f6d7
Kelly Kang <shkx281@gmail.com>
2022-05-08 23:30:37 -0700

    copy pasted answers

--- part-1.py
+++ part-1.py
@@ -5,16 +5,60 @@
 
 # factorial
 
-
+def factorial(n):
+    """
+    The base case is when n is 1, the factorial is 1.
+    
+    return the product of all the integers so far and n
+    """
+    if n < 0:
+        raise ValueError
+    
+    if n <= 1:
+        return 1
+        
+    return n * factorial(n-1)  
 
 # reverse
 
-
+def reverse(text):
+    """
+    base case: when len(text) == 1 return text
+    
+    add to the string of previous chars the last char and remove the last char.
+    """
+    
+    if len(text) <= 1:
+        return text
+    
+    return text[-1] + reverse(text[:-1]) 
 
 # bunny
 
+def bunny(count):
+    """
+    base case: when count == 0 return count
+    
+    return the total sum and two and decrease the count of bunny by 1
+    """
+    
+    if not count:
+        return count
+        
+    return bunny(count - 1) + 2
 
 
 # is_nested_parens
 
-
+def is_nested_parens(parens):
+    """
+    base case: when len(parens) == 0 return True
+    """
+    
+    if not parens:
+        return True
+        
+    if parens[0] != "(" or parens[-1] != ")":
+        return False
+    
+    return is_nested_parens(parens[1:-1])
--- part-2.py
+++ part-2.py
@@ -5,12 +5,49 @@
 
 # search
 
-
+def search(array, query):
+    """
+    base case: is not array return False
+    """
+    if not array:
+        return False
+    if array[-1] == query:
+        return True
+    return search(array[:-1], query)
 
 # is_palindrome
 
-
+def is_palindrome(text):
+    """
+    base case: if not text or text == 1 return True
+    """
+    if len(text) <= 1:
+        return True
+    if text[0] != text[-1]:
+        return False
+    return is_palindrome(text[1:-1])
 
 # digit_match
 
-
+def digit_match(a, b):
+    """ 
+    base case: if only one digit is left for either number
+    check if the digit is matching. Return 1 if matching, 0 if not. 
+    """
+    
+    
+    if a//10 == 0 or b//10 == 0:
+        if a % 10 == b % 10:
+            return 1
+        else:
+            return 0
+            
+    count = 0
+            
+    if a % 10 == b % 10:
+        count += 1
+        
+    count += digit_match(a // 10, b // 10)
+        
+    
+    return count
