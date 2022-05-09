8473f1221f4c7d51009f5affa06c9e9dd2b9f47d
Fena Dang <fenadang@gmail.com>
2022-05-08 22:51:22 -0700

    added pt1 and pt2

--- part-1.py
+++ part-1.py
@@ -4,17 +4,46 @@
 # the appropriate comment.
 
 # factorial
+def factorial(num):
+    if num < 0:
+        raise ValueError
+    if num == 0:
+        return 1
+    return num * factorial(num-1)
 
 
 
 # reverse
-
+def reverse(text):
+    if len(text) == 1:
+        return text
+    elif text == "":
+        return ""
+        
+    
+    return text[-1] + reverse(text[:-1])
 
 
 # bunny
+def bunny(count):
+    sum = 0
+    if count == 1:
+        return 2
+    elif count == 0:
+        return 0
+    sum += 2
+        
+    return sum + bunny(count-1)
 
 
 
 # is_nested_parens
+def is_nested_parens(parens):
+    if parens == "":
+        return True
+    elif parens[0] != "(" or parens[-1] != ")":
+        return False
+    
+    return is_nested_parens(parens[1:-1])
 
 
--- part-2.py
+++ part-2.py
@@ -4,13 +4,35 @@
 # the appropriate comment.
 
 # search
+def search(array, query):
+    if array == []:
+        return False
+    elif query == array[0]:
+        return True
+        
+    
+    return search(array[1:], query)
 
 
 
 # is_palindrome
+def is_palindrome(text):
+    if len(text) == 1:
+        return True
+    elif text[0] != text[-1]:
+        return False
+    
+    return is_palindrome(text[1:-1])
 
 
 
 # digit_match
+def digit_match(num1, num2):
+    if num1 == 0 and num2 == 0:
+        return 1
+    elif num1 == 0 or num2 == 0:
+        return 0
+
+#No clue how to write the rest of this function lol 
 
 
