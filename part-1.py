ad35f40f6dd434feb3841942f7caae7f371812b2
Morgan Adkisson <adkissonmorgan@gmail.com>
2022-05-09 13:28:56 -0700

    problem set answer submission

--- part-1.py
+++ part-1.py
@@ -4,17 +4,46 @@
 # the appropriate comment.
 
 # factorial
-
+def factorial(n):
+    # base case
+    if n == 0:
+        return 1
+    if n < 0:
+        raise ValueError
+    #recursive case
+    return n*factorial(n-1)
 
 
 # reverse
-
+def reverse(text):
+    # base case
+    if len(text) == 0:
+        return ""
+    elif len(text) == 1:
+        return text
+    # recursive case
+    return reverse(text[1:])+text[0]
 
 
 # bunny
-
+def bunny(count):
+    # base case
+    if count == 0:
+        return 0 
+    # validate
+    if count < 0 or count != int(count):
+        raise ValueError
+    # recursive case
+    return 2 + bunny(count-1)
 
 
 # is_nested_parens
-
+def is_nested_parens(parens): 
+    #base case
+    if len(parens) == 0: 
+        return True
+    elif parens[0] != "(" or parens[-1] != ")":
+        return False
+    #recursive case
+    return is_nested_parens(parens[1:-1])
 
--- part-2.py
+++ part-2.py
@@ -4,13 +4,40 @@
 # the appropriate comment.
 
 # search
-
+def search(array, query):
+    # base case 
+    if len(array) == 0:
+        return False
+    if array[0] == query:
+        return True
+    # recursive case
+    return search(array[1:], query)
 
 
 # is_palindrome
-
+def is_palindrome(text):
+    # base case
+    if len(text) == 1:
+        return True
+    # recursive case
+    elif text[0] == text[-1]:
+        return is_palindrome(text[1:-1])
+    return False
 
 
 # digit_match
-
+def digit_match(int_a, int_b):
+    # base case
+    if int_a == 0 and int_b == 0:
+        return 1
+    elif int_a < 10 or int_b < 10: 
+        if int_a % 10 == int_b % 10:
+            return 1
+        else:
+            return 0
+    # recursive case
+    if int_a % 10 == int_b % 10: 
+        return digit_match(int_a // 10, int_b // 10) + 1 
+    else:
+        return digit_match(int_a // 10, int_b // 10)
 
