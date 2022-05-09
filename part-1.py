3d36c70b3200760ddd878cc4ff304e42690373a5
Julie <jawarren89@gmail.com>
2022-05-09 00:35:52 -0700

    completes functions for part-1 and most of part-2

--- part-1.py
+++ part-1.py
@@ -4,17 +4,36 @@
 # the appropriate comment.
 
 # factorial
-
+def factorial(n):
+    if n < 0:
+        raise ValueError
+    elif n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
 
 
 # reverse
+def reverse(word):
+    if len(word) == 0:
+        return ""
 
+    return word[len(word)-1]+ reverse(word.rstrip(word[len(word)-1]))
 
 
 # bunny
-
+def bunny(n):
+    if n == 0:
+        return 0
+    return bunny(n-1) + 2
 
 
 # is_nested_parens
-
-
+def is_nested_parens(parens):
+    if len(parens) == 0:
+        return True
+    
+    if parens[0] == "(" and parens[-1] == ")":
+        return is_nested_parens(parens[1:-1])
+    else:
+        return False
--- part-2.py
+++ part-2.py
@@ -4,13 +4,37 @@
 # the appropriate comment.
 
 # search
-
+def search(array, query):
+    if len(array) == 0:
+        return False
+    if query == array[0]:
+        return True
+    else:
+        return search(array[1:], query)
 
 
 # is_palindrome
-
+def is_palindrome(word):
+    if len(word) <= 1:
+        return True
+    elif word[0] == word[-1]:
+        return is_palindrome(word[1:-1])
+    return False
 
 
 # digit_match
+# ** Works for all cases except initial case (0, 0) ***
+# ** Any variation I tried caused loss of functionality for other cases **
+def digit_match(int_1, int_2):
+    count = 0
+    if int_1 == 0 or int_2 == 0:
+        return count
 
+    if int_1 == 0 and int_2 == 0:
+        return 1
+    elif int_1 % 10 == int_2 % 10:
+        count += 1
+        return count + digit_match(int_1//10,int_2//10)
+    else:
+        return digit_match(int_1//10,int_2//10)
 
