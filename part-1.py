56f7fd517896159ad800789175601b5e5251a662
sarah <skiyoko14@gmail.com>
2022-05-08 22:54:39 -0700

    recursion solutions

--- part-1.py
+++ part-1.py
@@ -4,17 +4,46 @@
 # the appropriate comment.
 
 # factorial
+def factorial(n):
+    if n < 0: 
+        raise ValueError
+    
+    if n == 1 or n == 0: #ok I know (now) this can be just n == 0
+        #but I feel bad about changing my original solution
+        #after reading the explanation
+        return 1
+    
+    return factorial(n-1) * n
 
 
 
 # reverse
+def reverse(string):
+    if len(string) <= 1:
+        return string
 
+    return "".join([string[-1], reverse(string[1:-1:]), string[0]])
 
 
 # bunny
-
+def bunny(count):
+    if count == 0:
+        return 0
+    return bunny(count - 1) + 2
 
 
 # is_nested_parens
+def is_nested_parens(parens):
+    if parens == "":
+        return True
+    elif parens == "(" or parens == ")":
+        return False
+    return parens[0] == "(" \
+            and parens[-1] == ")" \
+            and is_nested_parens(parens[1:-1:])
+    ## In hindsight the first two lines of this return
+    # could be inside the function instead of in the
+    # return and it would be more clean
 
-
+    # I don't understand the what the challenge problem
+    # is asking for. What counts as a "new string"?
--- part-2.py
+++ part-2.py
@@ -4,13 +4,63 @@
 # the appropriate comment.
 
 # search
-
-
+def search(array, query):
+    if len(array) == 0:
+        return False
+    elif array[0] == query:
+        return True
+    else:
+        return search(array[1::], query)
 
 # is_palindrome
-
+def is_palindrome(text):
+    if len(text) <= 1:
+        return True
+    elif text[0] != text[-1]:
+        return False
+    else:
+        return is_palindrome(text[1:-1:])
 
 
 # digit_match
+def digit_match(int1, int2):
+    if int1 == 0 and int2 == 0:
+        return 1
+    elif int1 == 0 or int2 == 0:
+        return 0
+    elif int1 < 10 and int2 < 10:
+        if int1 % 10 == int2 % 10:
+            return 1
+        else:
+            return 0
+    elif int1 % 10 == int2 % 10:
+        return 1 + digit_match((int1 // 10), (int2//10))
+    else:
+        return digit_match((int1 // 10), (int2 // 10))
+# Emotionally I would rather make the question match "positive integers",
+# toss test 4, and write the solution
+# def digit_match(int1, int2):
+#     if int1 == 0 or int2 == 0:
+#         return 0
+#     elif int1 % 10 == int2 % 10:
+#         return 1 + digit_match((int1 // 10), (int2//10))
+#     else:
+#         return digit_match((int1 // 10), (int2 // 10))
+# which is much prettier... unless part of the question was learning
+# "handling edge cases using recursion is annoying"
+# which, fair, if so. (wish the hint was about that, though,
+# instead of about floor division!)
 
-
+## I had to solve the problem this way
+# def digit_match(int1, int2):
+#     int1 = str(int1)
+#     int2 = str(int2)
+#     if len(int1) == 0 or len(int2) == 0:
+#         return 0
+#     if int1[-1] == int2[-1]:
+#         return 1 + digit_match(int1[:-1], int2[:-1])
+#     else:
+#         return 0 + digit_match(int1[:-1], int2[:-1])
+## and look at the Ada explanation solution
+## to figure out how to handle the "match 0 to 0" case
+## ._."
