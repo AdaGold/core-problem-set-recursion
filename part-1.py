7ce572ff5bb81ff807ebb3c83e605cec5ff031c5
mit <mit.magh@gmail.com>
2022-05-09 08:53:47 -0700

    Done with part 1 problem set

--- part-1.py
+++ part-1.py
@@ -4,17 +4,39 @@
 # the appropriate comment.
 
 # factorial
-
+def factorial(n):
+    if n == 0:
+        return 1
+    if n<0:
+        raise ValueError
+    return n*factorial(n-1)
 
 
 # reverse
-
+def reverse(text):
+    if text == "":
+        return text
+    if len(text) <= 1:
+        return text
+        
+    return reverse(text[1:]) + text[:1]
 
 
 # bunny
-
+def bunny(count):
+    if count<=0:
+        return 0
+    if count==1:
+        return 2
+    return bunny(count-1) + 2
 
 
 # is_nested_parens
-
+def is_nested_parens(parens):
+    if len(parens)==0:
+        return True
+    elif parens[:1] != '(' or  parens[-1:] != ')':
+        return False
+    else:
+        return is_nested_parens(parens[1:-1])
 
