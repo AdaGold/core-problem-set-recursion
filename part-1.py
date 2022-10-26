b6523faa0de9490bfe1f303a9d9df51a1022d255
Margaret <y.margaret.lu+github@gmail.com>
2022-10-26 10:30:15 -0700

    added code from recursion to parts 1 and 2

--- part-1.py
+++ part-1.py
@@ -4,17 +4,34 @@
 # the appropriate comment.
 
 # factorial
-
-
+def factorial(n):
+    if n<0:
+        raise ValueError
+    if n == 0:
+        return 1
+    return n*factorial(n-1)
 
 # reverse
-
-
+def reverse(string):
+    if len(string)<=1:
+        return string
+    first = string[0]
+    other = string[1:]
+    return reverse(other)+first
 
 # bunny
-
-
+def bunny(num):
+    if num == 0:
+        return 0
+    return 2+bunny(num-1)
 
 # is_nested_parens
 
-
+def is_nested_parens(string):
+    print(string)
+    if string == "":
+        return True
+    if string =="((" or string=="))":
+        return False
+    if string[0]+string[-1]=="()":
+        return is_nested_parens(string[1:-1])
--- part-2.py
+++ part-2.py
@@ -5,12 +5,31 @@
 
 # search
 
-
+def search(a_list,chara):
+    if len(a_list)==0:
+        return False
+    if a_list[0]==chara:
+        return True
+    return search(a_list[1:],chara)
 
 # is_palindrome
 
-
+def is_palindrome(string):
+    if len(string)<2:
+        return True
+    if string[0]==string[-1]:
+        return is_palindrome(string[1:-1])
+    return False
 
 # digit_match
 
-
+def digit_match(a,b):
+    if a == 0 and b == 0:
+        return 1
+    elif a < 10 or b < 10:
+        if a % 10 == b % 10:
+            return 1
+        return 0
+    if a%10 == b%10:
+        return digit_match(a//10,b//10) + 1
+    return digit_match(a//10,b//10)
