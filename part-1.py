0ee0b32471f06b285d4f4703a2e2547569a2055d
hannahtan96 <hannahtan96@gmail.com>
2022-11-09 09:42:32 -0800

    added part 1 and 2

--- part-1.py
+++ part-1.py
@@ -4,17 +4,44 @@
 # the appropriate comment.
 
 # factorial
-
+def factorial(n):
+    if n < 0:
+        raise ValueError("Input must be greater than 0!")
+        
+    if n == 0:
+        return 1
+        
+    return factorial(n-1) * n
 
 
 # reverse
-
+def reverse(text):
+    if not text or len(text) == 1:
+        return text
+        
+    return text[-1] + reverse(text[:-1])
+        
 
 
 # bunny
+def bunny(count):
+    if not count:
+        return 0
+        
+    return 2 + bunny(count-1)
 
 
 
 # is_nested_parens
+def is_nested_parens(parens):
+    if not parens:
+        return True
 
+    if parens[0] == '(' and parens[-1] == ')':
+        parens = parens[1:-1]
+    else:
+        return False
+            
+        
+    return is_nested_parens(parens)
 
