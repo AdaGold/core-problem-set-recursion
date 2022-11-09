014e71478b1554fe641961e62d3efb4640fcadb7
Puja Mukherjee <pujam836@gmail.com>
2022-11-09 13:31:50 -0800

    recursion problems part1 and part2

--- part-1.py
+++ part-1.py
@@ -4,17 +4,33 @@
 # the appropriate comment.
 
 # factorial
-
-
+def factorial(n):
+    if n < 0:
+        raise ValueError
+    if n == 0 or n==1:
+        return 1
+    return n * factorial(n-1)
 
 # reverse
+def reverse(s):
+    if s=="":
+        return s
+    return reverse(s[1:])+s[0]
 
 
 
 # bunny
-
-
-
+def bunny(count):
+    if count == 0:
+        return 0
+    return 2+bunny(count-1)
+    
 # is_nested_parens
-
+def is_nested_parens(parens):
+    if parens == '()' or parens == "":
+        return True
+    elif parens[:1] != "(" or parens[-1:] != ")":
+        return False
+    else:
+        return is_nested_parens(parens[1:-1])
 
--- part-2.py
+++ part-2.py
@@ -4,13 +4,38 @@
 # the appropriate comment.
 
 # search
-
+def search(array, query):
+    if not array:
+        return False
+    elif array[0]==query:
+        return True
+    return search(array[1:],query)
 
 
 # is_palindrome
+def is_palindrome(text):
+    if len(text)<2:
+        return True
+    if text[0]!=text[-1]:
+        return False
+    return is_palindrome(text[1:-1])
 
 
 
 # digit_match
+def digit_match(digit1,digit2):
+    if digit1==0 and digit2==0: #edge case both are 0 and 1 digit
+        return 1
+    #if one or both are 1 digit
+    elif digit1//10==0 or digit2//10==0:
+        if digit1%10==digit2%10:
+            return 1
+        else:
+            return 0
+    
+    elif digit1%10==digit2%10:
+        return 1+digit_match(digit1//10, digit2//10)
+    return digit_match(digit1//10, digit2//10)
+        
 
 
