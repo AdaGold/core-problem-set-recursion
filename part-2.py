285f9f4eea84695cb0ca72a015e59013aeff5a68
masinakh <nakhjirimasi@gmail.com>
2022-11-09 13:21:04 -0800

    part1 -part 2 recursion is finished

--- part-2.py
+++ part-2.py
@@ -4,13 +4,42 @@
 # the appropriate comment.
 
 # search
+def search(array,query):
+    if len(array) == 0:
+        return False
+        
+    elif array[0] == query:
+        return True
+        
 
 
 
 # is_palindrome
-
+def is_palindrome(text):
+    if len(text) == 1:
+        return True
+    elif text[0] != text [-1]:
+        return False
+    return is_palindrome(text[1:-1])  
 
 
 # digit_match
+def digit_match(num1, num2):
+    if num1 ==0 and num2 == 0:
+        return 1
+    
+    return digit_match_helper(num1, num2,0)
+    
+    
+        
+def digit_match_helper(num1,num2,count):
+    if num1 == 0 or num2 == 0:
+        return count
+        
+    if num1 % 10 == num2 % 10:
+        return digit_match_helper(num1 //10 , num2 //10 ,count+1)
+        
+    if num1 % 10  != num2 % 10:  
+        return digit_match_helper(num1 //10 , num2 //10 ,count)
 
 
