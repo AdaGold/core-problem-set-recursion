06dbb8f2478b78145096c77e8d1cdb83936c2ab0
Tasha <tashachang2@gmail.com>
2022-11-08 21:01:16 -0800

    completed functions 1-3 of part 1

--- part-1.py
+++ part-1.py
@@ -4,11 +4,20 @@
 # the appropriate comment.
 
 # factorial
-
+def factorial(num):
+    if num < 0:
+        raise ValueError
+    if num == 0:
+        return 1
+    return factorial(num-1) * num
 
 
 # reverse
+def reverse(text):
+    if len(text) == 0:
+        return text
 
+    return reverse(text[1:]) + text[0]
 
 
 # bunny
