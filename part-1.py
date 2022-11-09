f8a6c7208b721fe83866804c73be8db460d5c4ab
Marilyn Thai <marilynthai15@gmail.com>
2022-11-08 23:24:26 -0800

    added all solutions to files

--- part-1.py
+++ part-1.py
@@ -4,17 +4,37 @@
 # the appropriate comment.
 
 # factorial
-
+def factorial(n):
+    if n == 0:
+        return 1
+    if n < 0 :
+        raise ValueError
+    return n * factorial(n-1)
 
 
 # reverse
-
+def reverse(text):
+    if len(text) == 0:
+        return text
+    return reverse(text[1:]) + text[0]
 
 
 # bunny
-
+def bunny(count):
+    if count == 0:
+        return 0
+    return bunny(count -1) + 2
 
 
 # is_nested_parens
-
+def is_nested_parens(parens):
+    if parens == "":
+        return True
+    inner_parens = parens.find("()")
+    if inner_parens != -1:
+        new_parens = parens[:inner_parens] + parens[inner_parens+2:]
+        return is_nested_parens(new_parens)
+    else:
+        return False
+    
 
--- part-2.py
+++ part-2.py
@@ -4,13 +4,38 @@
 # the appropriate comment.
 
 # search
-
+def search(array,query):
+    if len(array) == 1:
+        return array[0] == query
+    elif len(array) == 0:
+        return False
+    if array[0] == query:
+        return True
+    array.pop(0)
+    return search(array,query)
+    
 
 
 # is_palindrome
-
+def is_palindrome(text):
+    if len(text) <= 1:
+        return True
+    if text[0] == text[-1]:
+        return is_palindrome(text[1:-1])
+    return False
 
 
 # digit_match
-
+def digit_match(n,k):
+    if n == 0 and k == 0:
+        return 1
+    else:
+        result = 0 
+        if n == k%10 or k == n%10:
+            return result+1
+        elif n//10 == 0 or k//10 ==0:
+            return result
+        elif n%10 == k%10:
+            result += 1    
+        return result + digit_match(n//10,k//10)
 
