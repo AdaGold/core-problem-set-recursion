5c7aff43f627d0563fba4e20133d1c072f62d0f0
Milena Souto <yamadasouto@gmail.com>
2022-11-08 20:51:11 -0800

    Add 3 solutions for part 1

--- part-1.py
+++ part-1.py
@@ -4,17 +4,38 @@
 # the appropriate comment.
 
 # factorial
+def factorial(n):
+    if n < 0:
+        raise ValueError("Can't use negative numbers")
 
+    if n == 1:
+        return 1
 
+    return n * factorial(n - 1)
 
 # reverse
 
 
+def reverse(text):
+    if len(text) <= 1:
+        return text
+    return reverse(text[1:]) + text[0]
 
 # bunny
 
 
+def bunny(count):
+    if count == 0:
+        return 0
+
+    return 2 + bunny(count - 1)
+
 
 # is_nested_parens
-
-
+def is_nested_parens(parens):
+    # need to count open
+    def count(string, chart_to_count):
+        # parens, ")"
+        # if open != closed
+        pass
+    pass
