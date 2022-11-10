53a139cf4b5a33c13ea67427f1c1f9ed533ef20d
Younghee <kwak.younghee@gmail.com>
2022-11-09 19:25:27 -0800

    recursion part 1: 3 passed, last 1 did not pass

--- part-1.py
+++ part-1.py
@@ -3,18 +3,46 @@
 # Please paste your solution underneath
 # the appropriate comment.
 
-# factorial
+# These solutions are based on the lecture I took on 11/09/2022
+# Tried to understand recursion anyway!
 
+# factorial
+def factorial(n):
+    
+    if n == 0:
+        return 1
+    if n < 0:
+        raise ValueError
+        
+    return n * factorial(n-1)
 
 
 # reverse
+def reverse(text):
 
+    if text == '':
+        return text
+    else:
+        return reverse(text[1:]) + text[0]
 
 
 # bunny
-
+def bunny(n):
+    
+    if n <= 0:
+        return 0
+        
+    return 2 + bunny(n-1)
 
 
 # is_nested_parens
+# does not work yet
+def is_nested_parens(parens):
+    if parens == "":
+        return True
+    elif len(parens) == 1:
+        return False
+    else:
+        return is_nested_parens(parens[1:-1])
 
 
