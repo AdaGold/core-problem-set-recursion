a3c1935cce7408925362b0b9b23b8ac429a5a9ce
lisafeng <lisafenglisa@yahoo.com>
2022-11-09 21:13:21 -0500

    solutions from problem set part1 and part2

--- part-1.py
+++ part-1.py
@@ -4,17 +4,35 @@
 # the appropriate comment.
 
 # factorial
-
+def factorial(num):
+    if num < 0:
+        raise ValueError
+    if num == 0:
+        return 1
+    return factorial(num-1) * num
 
 
 # reverse
-
+def reverse(s):
+    if len(s) == 0:
+        return s
+    return reverse(s[1:]) + s[0]
 
 
 # bunny
-
+def bunny(count):
+    if count == 0:
+        return 0
+    
+    return bunny(count-1) + 2
 
 
 # is_nested_parens
-
+def is_nested_parens(parens):
+    if parens == "":
+        return True
+    if parens[0] == parens[-1]:
+        return False
+    
+    return is_nested_parens(parens[1:-1])
 
--- part-2.py
+++ part-2.py
@@ -4,13 +4,51 @@
 # the appropriate comment.
 
 # search
-
+def search(array,query):
+    if len(array) == 0:
+        return False
+    elif query == array[0]:
+        return True
+    else:
+        return search(array[1:], query)
 
 
 # is_palindrome
-
+def is_palindrome(text):
+    if text[0] != text[-1]:
+        return False
+    elif len(text) == 1:
+        return True
+    return is_palindrome(text[1:-1])
 
 
 # digit_match
+def digit_match(num1, num2):
+    if num1 == 0 and num2 == 0:
+        return 1
+    elif num1 < 10 or num2 < 10:
+        if num1 % 10 == num2 % 10:
+            return 1
+        else:
+            return 0
+    
+    if num1 % 10 == num2 % 10:
+        return 1 + digit_match(num1 // 10, num2 // 10)
+    else:
+        return digit_match(num1 // 10, num2 // 10)
 
-
+    # non-recursion solution (I couldn't figure out how to do it using recursion, so I did a non-recursion solution first.)
+    
+    # s1 = str(num1)[::-1]
+    # s2 = str(num2)[::-1]
+    # count = 0
+    
+    # if len(s1) < len(s2):
+    #     length = len(s1)
+    # else:
+    #     length = len(s2)
+        
+    # for i in range(length):
+    #     if s1[i] == s2[i]:
+    #         count += 1
+    # return count
