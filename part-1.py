04d030ac61a755ea981a89594ac3e833e282a70e
takhat <takhat014@gmail.com>
2022-11-12 22:42:03 -0800

    added functions

--- part-1.py
+++ part-1.py
@@ -4,17 +4,35 @@
 # the appropriate comment.
 
 # factorial
-
+def factorial(n):
+    if n < 0: 
+        raise ValueError("Please enter a positive integer.")
+    elif n < 2:
+        return 1
+    return n * factorial(n - 1)
 
 
 # reverse
-
+def reverse(text):
+    if len(text) == 0:
+        return text
+    return reverse(text[1:]) + text[0]
 
 
 # bunny
-
+def bunny(count):
+    if count == 0:
+        return 0
+    
+    return 2 + bunny(count-1)
 
 
 # is_nested_parens
+def is_nested_parens(parens):
+    if len(parens) == 0:
+        return True
+    elif "()" not in parens:
+        return False
+    return is_nested_parens(parens[1:-1])
 
 
