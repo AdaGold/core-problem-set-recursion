e502ba83f9abf9e2d79a75725eebb06944b8f585
Shweet Abraha <sabraha0311@gmail.com>
2022-11-13 18:32:42 -0800

    Soltions for problems part 1 complete: factorial, reverse, bunny, is_nested_parens. Soltions for problems part 2 complete: search, is_palindrome, digit_match.

--- part-1.py
+++ part-1.py
@@ -5,16 +5,43 @@
 
 # factorial
 
+def factorial(n):
+    if n<0:
+        raise ValueError
+    elif n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+
 
 
 # reverse
+
+def reverse(text):
+    if len(text)==0:
+        return ''
+    elif len(text)==1:
+        return text
+    return reverse(text[1:])+text[0]
 
 
 
 # bunny
 
+def bunny(count):
+    if count == 0:
+        return 0
+    return 2 + bunny(count-1)
 
 
 # is_nested_parens
+
+def is_nested_parens(parens):
+    if len(parens) == 0:
+        return True
+    elif parens[:1] != "(" or parens[-1:] != ")":
+        return False
+    else:
+        return is_nested_parens(parens[1:-1])
 
 
--- part-2.py
+++ part-2.py
@@ -5,12 +5,49 @@
 
 # search
 
+def search(array, query):
+    if len(array) == 0:
+        return False
+    elif array[0] == query:
+        return True
+        
+    return search(array[1:], query)
+
 
 
 # is_palindrome
 
+def is_palindrome(text):
+    if len(text) <= 1:
+        return True
+    
+    if text[0] != text[-1]:
+        return False
+        
+    return is_palindrome(text[1:-1])
 
 
 # digit_match
+
+def digit_match(a, b):
+    if a == 0 and b == 0:
+        return 1
+    
+    elif a < 10 or b < 10:
+        if a % 10 == b % 10:
+            return 1
+        else: 
+            return 0
+            
+    last_digit_a = a % 10
+    last_digit_b = b % 10
+    
+    remaining_a = a // 10
+    remaining_b = b // 10
+    
+    if last_digit_a == last_digit_b:
+        return 1+digit_match(remaining_a, remaining_b)
+        
+    return digit_match(remaining_a, remaining_b)
 
 
