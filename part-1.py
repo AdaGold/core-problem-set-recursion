f978488b66cf0f7f3a7c1b2cc7165d05acdd43c6
Andrea-Galindo <andreagalindo.guerra@gmail.com>
2022-11-15 17:08:25 -0500

    added solutions recursion part-1

--- part-1.py
+++ part-1.py
@@ -4,17 +4,39 @@
 # the appropriate comment.
 
 # factorial
-
+def factorial(n):
+    if n == 0:
+        return 1
+    elif n < 0:
+        raise ValueError
+        
+    return factorial(n - 1) * n 
 
 
 # reverse
-
+def reverse(text):
+    if text == "":
+        return text
+    else:
+        return reverse(text[1:]) + text[0]
 
 
 # bunny
-
+def bunny(count):
+    if not count:
+        return count
+        
+    return 2 + bunny(count - 1)
 
 
 # is_nested_parens
+def is_nested_parens(parens):
+    if parens == "":
+        return True
+        
+    if parens[0] == "(" and parens[-1] == ")":
+        return is_nested_parens(parens[1:-1])
+    
+    return False
 
 
