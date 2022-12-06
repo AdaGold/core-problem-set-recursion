350d7d55dd1a9ebdf31045bd9da5df1479c4749e
brookestrz <113567961+brookestrz@users.noreply.github.com>
2022-12-05 20:06:32 -0500

    Update part-1.py

--- part-1.py
+++ part-1.py
@@ -4,17 +4,41 @@
 # the appropriate comment.
 
 # factorial
+def factorial(n):
+    if n == 0:
+        return 1
+    elif n < 0:
+        raise ValueError()
+    
+    return n * factorial(n-1)
 
 
 
 # reverse
+def reverse(text):
+    if len(text) == 0:
+        return text
+    return reverse(text[1:]) + text[0]
+    
 
 
 
 # bunny
+def bunny(count):
+    if count == 0:
+        return 0
+    return bunny(count-1) + 2 
+    
 
 
 
 # is_nested_parens
+def is_nested_parens(parens):
+    if len(parens) == 0:
+        return True
+    if parens[:1] != "(" or parens[-1:] != ")":
+        return False
+    else:
+        return is_nested_parens(parens[1:-1])
 
 
