52059dc622f8f57e8ec846c5ef156e13d06dfc6b
Adie_lady <50897076+sikasarpong@users.noreply.github.com>
2022-12-28 12:11:23 -0800

    Update part-2.py

--- part-2.py
+++ part-2.py
@@ -22,5 +22,17 @@ def is_palindrome(s):
         return word[0] == word[-1] and is_palindrome(word[1:-1])
 
 # digit_match
-
+def digit_match(first_num, sec_num):
+    if first_num == 0 and sec_num == 0:
+        return 1
+    elif first_num < 10 or sec_num < 10:
+        if first_num % 10 == sec_num% 10:
+            return 1
+        
+        return 0
+        
+    if first_num % 10 == sec_num % 10:
+        return 1 + digit_match(first_num // 10, sec_num // 10)
+        
+    return digit_match(first_num// 10, sec_num // 10)
 
