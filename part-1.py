4f2b609dd8db874ef4eaa65718ec783dd6905e2e
Adie_lady <50897076+sikasarpong@users.noreply.github.com>
2022-12-28 11:36:58 -0800

    Update part-1.py

--- part-1.py
+++ part-1.py
@@ -4,17 +4,37 @@
 # the appropriate comment.
 
 # factorial
+def factorial(n):
+    if n < 0:
+        raise ValueError("Number factorial ends at one")
+    elif n == 0:
+        return 1
 
+    return n * factorial(n-1)
 
 
 # reverse
-
+def reverse(text):
+    if text == "":
+        return text
+    else:
+        return reverse(text[1:]) + text[0]
 
 
 # bunny
 
-
+def bunny(count):
+    if not count:
+        return count
+    else:
+        return 2 + bunny(count -1)
 
 # is_nested_parens
 
-
+def is_nested_parens(parens):
+    if len(parens) == 0:
+        return True
+    if parens[:1] != "(" or parens[-1:] != ")":
+        return False
+    else:
+        return is_nested_parens(parens[1:-1])
