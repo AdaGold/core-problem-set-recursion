2f5f04895495f2039fc39275432a85361c992a27
maple-megan333 <maple.megan333@gmail.com>
2023-01-09 16:50:28 -0800

    Finished Recursion

--- part-1.py
+++ part-1.py
@@ -4,17 +4,46 @@
 # the appropriate comment.
 
 # factorial
-
+def factorial(num):
+    if num < 0:
+        raise ValueError
+    if num == 0:
+        return 1
+    return factorial(num-1)*num
 
 
 # reverse
+def reverse(string):
+    if len(string) == 0:
+        return string
+    return reverse(string[1:])+string[0]
 
 
 
 # bunny
+def bunny(bunnies):
+    if bunnies==0:
+        return 0
+    return 2+bunny(bunnies-1)
 
 
 
 # is_nested_parens
+def compare(string, front, back):
+    parens = {
+        "(":")"
+    }
+    if front>=back:
+        return True
+    if parens.get(string[front])!=string[back]:
+        return False
+    elif front < (back+1):
+        return compare(string, front+1, back-1)
+        
+def is_nested_parens(string):
+    end = len(string) -1
+    if len(string) == 0:
+        return True
+    return compare(string, 0, end)
 
 
--- part-2.py
+++ part-2.py
@@ -4,13 +4,46 @@
 # the appropriate comment.
 
 # search
+def compare(iterable, n,query):
+    if n==len(iterable):
+        return False
+    if iterable[n] == query:
+        return True
+    return compare(iterable, n+1, query)
 
+def search(to_search, query):
+    return compare(to_search,0,query)
 
 
 # is_palindrome
+def compare(string, a, b):
+    if a >=b:
+        return True
+    if string[a] != string[b]:
+        return False
+    return compare(string, a+1, b-1)
+
+def is_palindrome(string):
+    if len(string)==0:
+        return True
+        
+    b=len(string)-1
+    return compare(string, 0, b)
+    
 
 
 
 # digit_match
+def digit_match(apples,oranges):
+    if apples==oranges: return len(str(apples))
+    elif apples==0 or oranges ==0: return 0
+    elif apples<10 and oranges<10:
+        if apples== oranges:return 1
+        return 0
 
-
+    a=apples%10
+    b=oranges%10
+    num = 0
+    if a==b:
+        num +=1
+    return num+ digit_match(apples//10, oranges//10)
