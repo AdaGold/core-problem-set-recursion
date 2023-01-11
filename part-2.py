c6652d5d30706ae07dccad190d35b342b4d667ed
mckay <mckaybwarren@gmail.com>
2023-01-10 20:02:18 -0500

    add solns for PS1 and PS2

--- part-2.py
+++ part-2.py
@@ -4,13 +4,36 @@
 # the appropriate comment.
 
 # search
-
+def search(array, query):
+    if not array:
+        return False
+    if query == array[0]:
+        return True
+    return search(array[1:], query)
 
 
 # is_palindrome
-
+def is_palindrome(text):
+    if not text:
+        return True
+    l = text[0]
+    r = text[-1]
+    if l != r:
+        return False
+    else:
+        return is_palindrome(text[1:-1])
 
 
 # digit_match
-
-
+def digit_match(num1, num2):
+    if num1 < 10 or num2 < 10:
+        if (num1 % 10) == (num2 % 10):
+            return 1
+        else:
+            return 0
+            
+    else:
+        if (num1 % 10) == (num2 % 10):
+            return 1 + digit_match((num1 // 10), (num2 // 10))
+        else:
+            return 0 + digit_match((num1 // 10), (num2 // 10))
