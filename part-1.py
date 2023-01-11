f6b9a30407ba7bb421a34cc13f6de1863cb7b104
Sheung Wan Wong <melodywonghxy@hotmail.com>
2023-01-11 10:50:05 -0700

    complete part-1

--- part-1.py
+++ part-1.py
@@ -4,17 +4,39 @@
 # the appropriate comment.
 
 # factorial
-
+def factorial(n):
+    if n == 0:
+        return 1
+    if n < 0:
+        raise ValueError
+    return n * factorial(n-1)
 
 
 # reverse
-
+def reverse(text):
+    if text == "":
+        return ""
+    return reverse(text[1:]) + text[0]
+    
 
 
 # bunny
+def bunny(count):
+    if count == 0:
+        return 0
 
+    return 2 + bunny(count-1)
 
 
 # is_nested_parens
-
+def is_nested_parens(parens):
+    if not parens:
+        return True
+    n = len(parens)
+    if n % 2 != 0:
+        return False
+    if parens[0] != "(" or parens[-1] != ")":
+        return False
+    return is_nested_parens(parens[1:-1])
+    
 
