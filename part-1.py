4c6659a8d9a26cc92cf6f4cb2956643b7d154a73
diana <arreoladiana11@gmail.com>
2023-01-10 18:51:07 -0800

    add problem sets

--- part-1.py
+++ part-1.py
@@ -4,17 +4,48 @@
 # the appropriate comment.
 
 # factorial
+def factorial(num):
+    if num < 0:
+        raise ValueError()
+    if num == 0:
+        return 1
 
+    return num*factorial(num-1)
 
 
 # reverse
-
+def reverse(word):
+    if not word:
+        return ""
+    if len(word) == 1:
+        return word
+    
+    
+    return reverse(word[1:]) + word[0]
 
 
 # bunny
-
+def bunny(count):
+    if count == 0:
+        return 0
+    if count == 1:
+        return 2
+    
+    return 2 + bunny(count-1)
+    
 
 
 # is_nested_parens
-
-
+def is_nested_parens(parens):
+    parens_dict = {")": "("}
+    
+    if not parens:
+        return True
+    
+    if len(parens) == 1:
+        return False
+    
+    if parens[0] != parens_dict[parens[-1]]:
+        return False
+        
+    return is_nested_parens(parens[1:-1])
--- part-2.py
+++ part-2.py
@@ -4,13 +4,59 @@
 # the appropriate comment.
 
 # search
-
+def search(array, query):
+    
+    if not query.isalpha():
+        return False
+    
+    if not array:
+        return False
+        
+    if array[0] == query:
+        return True
+    
+    return array[1:]
 
 
 # is_palindrome
-
+def is_palindrome(text):
+    
+    # if not text:
+    #     return False
+        
+    if len(text) == 1:
+        return True
+        
+    if text[0] != text[-1]:
+        return False
+    
+    return is_palindrome(text[1:-1])
 
 
 # digit_match
 
-
+def digit_match(num1, num2):
+    if num1 < 0 or num2 < 0:
+        return 0
+        
+    if num1 == 0 and num2 == 0:
+        return 1
+        
+    if num1 == 0 or num2 == 0:
+        return 0
+        
+    str_num1 = str(num1)
+    str_num2 = str(num2)
+    
+    new_num1 = 0 if not str_num1[:-1] else int(str_num1[:-1])
+    new_num2 = 0 if not str_num2[:-1] else int(str_num2[:-1])
+    
+    
+    if str_num1[-1] == str_num2[-1]:
+        return 1 + digit_match(new_num1, new_num2)
+        
+    if new_num1 == 0 and new_num2 == 0:
+        return 0
+    
+    return 0 + digit_match(new_num1, new_num2)
+        
