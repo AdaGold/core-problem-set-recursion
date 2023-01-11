1a82ee35eef2611ab3251161f74c21cb4098f593
annadu <duyuan0921@gmail.com>
2023-01-10 22:02:06 -0800

    add digit_match

--- part-2.py
+++ part-2.py
@@ -23,5 +23,19 @@ def is_palindrome(text):
 
 
 # digit_match
+def digit_match(apples, oranges):
 
+    if apples == 0 and oranges == 0:
+        return 1
+    
+    elif apples < 10 or oranges< 10:
+        if apples % 10 == oranges % 10:
+            return 1
+        
+        return 0
+    
+    if apples % 10 == oranges % 10:
+        return 1 + (digit_match(apples // 10, oranges // 10))
+        
+    return digit_match(apples// 10, oranges // 10)
 
