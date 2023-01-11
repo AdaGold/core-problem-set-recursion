27b9f381022bfde76052f8f1d6ab66a8b78c4ef6
Cindy Truong <cindythitruong@gmail.com>
2023-01-11 09:21:17 -0800

    init commit

--- part-1.py
+++ part-1.py
@@ -4,17 +4,51 @@
 # the appropriate comment.
 
 # factorial
-
+def factorial(n):
+    if n <= -1:
+        raise ValueError
+    #base case
+    if n == 0:
+        return 1
+    return n * factorial(n - 1)
 
 
 # reverse
-
+def reverse(text):
+    if len(text) == 0:
+        return text
+    else:
+        return reverse(text[1:])+text[0]
+    
 
 
 # bunny
-
+def bunny(count):
+    #base case
+    if count == 0:
+        return 0
+    if count == 1:
+        return 2
+    return 2 + bunny(count-1)
 
 
 # is_nested_parens
-
+def is_nested_parens(parens):
+    if parens == "":
+        return True
+    if len(parens)%2 != 0:
+        return False
+    
+    #finds ending paren -> ')' in parens params
+    ending_parens = parens.find(')')
+    #finding starting paren -> ('(') looking from right to left
+    starting_parens = parens[:ending_parens].rfind('(')
+    
+    if starting_parens == -1:
+        return False
+    
+    return is_nested_parens(
+        parens[:starting_parens] + parens[ending_parens + 1:]
+    )
+    
 
--- part-2.py
+++ part-2.py
@@ -4,13 +4,36 @@
 # the appropriate comment.
 
 # search
+def search(array, query):
+    if array == []:
+        return False
 
+    return query == array[0] or search(array[1:], query)
 
 
 # is_palindrome
-
+def is_palindrome(text):
+    if len(text) < 1:
+        return True
+    if text[0] != text[-1]:
+        return False
+    return is_palindrome(text[1:-1])
 
 
 # digit_match
-
+def digit_match(apples, oranges):
+    #checks if inputs are both 0
+    if apples == oranges:
+        return 1
+    #checks if either inputs are one digit nums
+    if apples < 10 or oranges < 10:
+        if apples % 10 == oranges % 10:
+            return 1
+        return 0
+    one = apples // 10
+    two = oranges // 10
+    
+    if apples % 10 == oranges % 10:
+        return digit_match(one, two) + 1
+    return digit_match(one, two)
 
