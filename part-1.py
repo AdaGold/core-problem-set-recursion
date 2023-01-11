b8c286f60515b98a3f28c773ced0c2b7bd926936
Catherine Bandarchuk <kate@sealucky.com>
2023-01-11 09:34:48 -0800

    Solved 3/7 problems.

--- part-1.py
+++ part-1.py
@@ -4,16 +4,26 @@
 # the appropriate comment.
 
 # factorial
-
-
+def factorial(number):
+    if number <= 1:
+        if number < 0:
+            raise ValueError("Negative Number")       
+        return 1
+    return number*factorial(number-1)
 
 # reverse
-
-
+def reverse(text):
+    if len(text) == 0:
+        return text
+    return reverse(text[1:]) + text[0]
 
 # bunny
-
-
+def bunny(count):
+    if not count:
+        return 0
+    if count == 1:
+        return 2
+    return 2 + bunny(count-1)
 
 # is_nested_parens
 
