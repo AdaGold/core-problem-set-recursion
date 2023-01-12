5cf82fd222440fc751a8f2cb4b2b5fd4c0a993ef
Jennifer Dai <jendai173@gmail.com>
2023-01-11 19:53:40 -0800

    completed exerience

--- part-1.py
+++ part-1.py
@@ -4,17 +4,44 @@
 # the appropriate comment.
 
 # factorial
-
+def factorial(n):
+    if n < 0:
+        raise ValueError
+    elif n == 0:
+        return 1
+    elif n == 1:
+        return n
+    return n * factorial(n-1)
 
 
 # reverse
-
+def reverse(string):
+    if len(string) == 1:
+        return string
+    elif len(string) == 0:
+        return ""
+    
+    temp = string[0]
+    return reverse(string[1:]) + temp
 
 
 # bunny
-
+def bunny(count):
+    if count == 0:
+        return 0
+    elif count == 1:
+        return 2
+        
+    return 2 + bunny(count-1)
 
 
 # is_nested_parens
-
+def is_nested_parens(parens):
+    
+    if parens == "":
+        return True
+    elif len(parens)%2 != 0:
+        return False
+    elif parens[0] =="(" and parens[len(parens)-1] ==")":
+        return True and is_nested_parens(parens[1:len(parens)-1])
 
--- part-2.py
+++ part-2.py
@@ -4,13 +4,34 @@
 # the appropriate comment.
 
 # search
+# def search(array,query):
+#     if query in array:
+#         return True
 
+def search(array,query):
+    if not len(array):
+        return False
+    elif array[0] == query:
+        return True
+    return search(array[1:],query)
 
 
 # is_palindrome
-
+def is_palindrome(text):
+    if len(text) < 2:
+        return True
+    elif text[0] != text[len(text)-1]:
+        return False
+    return is_palindrome(text[1:-1])
 
 
 # digit_match
-
+def digit_match(a1,a2):
+    if a1 <10 and a2 <10:
+        return int(a1 == a2)
+    
+    if a1 <10 or a2 <10:
+        return int(a1%10 == a2%10)
+        
+    return digit_match(a1//10, a2//10) + int(a1%10==a2%10)
 
