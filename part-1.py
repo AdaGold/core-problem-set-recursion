820c6f3101858efeb90067ac8022afb026f8da20
Eva Liu <una229@gmail.com>
2023-01-11 23:29:13 -0500

    part-1 and part-2 recursion

--- part-1.py
+++ part-1.py
@@ -4,17 +4,66 @@
 # the appropriate comment.
 
 # factorial
+def factorial(n):
+    if n < 0:
+        raise ValueError("ValueError") 
+        
+    if n == 0:
+        return 1
+    
+    return n*factorial(n-1) 
 
-
+print(factorial(5))
 
 # reverse
+def reverse(text):
+    if type(text) != str:
+        raise TypeError("TypeError")
+    
+    if len(text) == 0:
+        return text
+        
+    return text[-1] + reverse(text[:len(text)-1])
 
-
+print(reverse("cat"))
 
 # bunny
+def bunny(count):
+    if type(count) != int:
+        raise TypeError("TypeError")
+        
+    if count < 0:
+        raise ValueError("ValueError")
+    
+    if count == 0:
+        return 0
+        
+    return 2 + bunny(count-1)
 
-
+print(bunny(3))
 
 # is_nested_parens
+def is_nested_parens(parens, i=0, cnt=0):
+    if type(parens) != str:
+        raise TypeError("TypeError")
 
+    if parens == "":
+        return ""
+    
+    if len(parens) == 0:
+        return True
+    elif parens[:1] != "(" or parens[-1:] != ")":
+        return False
+    else:
+        return is_nested_parens(parens[1:-1])
+    
+    #other ---
+    #def is_nested_parens(parens, i=0, cnt=0)
+        # if i == len(parens): return cnt==0
+        # if cnt<0:return False
+        # if parens[i] == "(": return is_nested_parens(parens, i+1, cnt+1)
+        # elif parens[i] == ")": return is_nested_parens(parens, i+1, cnt-1)
+        # return is_nested_parens(parens, i+1, cnt)
+
+print(is_nested_parens("(())"))
 
--- part-2.py
+++ part-2.py
@@ -4,13 +4,56 @@
 # the appropriate comment.
 
 # search
+def search(arr,q):
+    if len(arr) == 0:
+        return False
+    
+    elif arr[0] == q:
+        return True
+    
+    arr.pop(0)   
+    return search(arr,q)
 
-
+print(search(["b", "c", "a"], "a"))
 
 # is_palindrome
+def is_palindrome(text):
+    if type(text) != str:
+        raise TypeError("TypeError")
+    
+    if len(text) == "" or len(text) == 1:
+        return True
+    elif text[:1] != text[-1:]:
+        return False
+        
+    return is_palindrome(text[1:-1])
 
 
+print( is_palindrome("racecar"))
 
 # digit_match
+def digit_match(num1, num2, count=0):
+    if num1 == 0 and num2 == 0:
+        return 1
+    
+    if num1 == 0 or num2 == 0:
+        return count
+    
+    if num1 == num2:
+        count += 1
+    elif num1 < 10:
+        if num1 == (num2 % 10):
+            count += 1
+    elif num2 < 10:
+        if num2 == (num1 % 10):
+            count += 1
+    elif num1 % 10 == num2 % 10:
+            count += 1
+        
+    return digit_match(num1//10, num2//10, count)
 
 
+
+num1 = 1 #=> 107250389.1 => 1
+num2 = 12 #=> 6253084.1 => 1
+print(digit_match(num1, num2))
