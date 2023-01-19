1b42eea506cf0d7d9d20cd36d69b0a202cd227b3
Anjula Tamang <parkhrinj@gmail.com>
2023-01-19 13:55:44 -0500

    Part 1 solutions

--- part-2.py
+++ part-2.py
@@ -4,11 +4,22 @@
 # the appropriate comment.
 
 # search
-
+def search(array,query):
+    if not array:
+        return False
+    if array[0] == query:
+        return True
+    return search(array[1:], query)
 
 
 # is_palindrome
-
+def is_palindrome(text):
+    if len(text) == 1:
+        return True
+    if text[0] != text[-1]:
+        return False
+    return is_palindrome(text[1:-1])
+    
 
 
 # digit_match
