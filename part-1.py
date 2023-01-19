be9aa293496965d5606d3f1906378e6ad04c6e9d
larissa <larza.ault@gmail.com>
2023-01-19 09:34:24 -0800

    Added solutions to recursive problems

--- part-1.py
+++ part-1.py
@@ -4,17 +4,44 @@
 # the appropriate comment.
 
 # factorial
-
+def factorial(n):
+    # handle invalid n
+    if n < 0:
+        raise ValueError
+    # base case
+    if n == 0:
+        return 1
+    # recursive case
+    return n * factorial(n-1)
 
 
 # reverse
-
+def reverse(text):
+    # handle empty string & single character string
+    if len(text) <= 1:
+        return text
+    # recursive case
+    return text[-1] + reverse(text[:-1])
 
 
 # bunny
-
+def bunny(count):
+    # base case
+    if count == 0:
+        return 0
+    # recursive case
+    return 2 + bunny(count-1)
 
 
 # is_nested_parens
-
-
+def is_nested_parens(parens):
+    # base cases
+    if len(parens) == 0:
+        return True
+    if len(parens) == 1:
+        return False
+    # recursive case
+    if parens[0] + parens[-1] == "()":
+        parens = parens[1:] # remove front (
+        parens = parens[:-1] # remove back )
+        return is_nested_parens(parens)
--- part-2.py
+++ part-2.py
@@ -4,13 +4,54 @@
 # the appropriate comment.
 
 # search
-
+def search(array, query):
+    i = 0
+    # base case
+    if i >= len(array):
+        return False
+    elif array[i] == query:
+        return True
+    else:
+        return search(array[1:], query)
 
 
 # is_palindrome
+def is_palindrome(text):
+    # base cases
+    if len(text) == 0:
+        return True
+    elif text[0] != text[-1]:
+        return False
+    else:
+        # recursive case
+        return is_palindrome(text[1:-1])
 
 
 
 # digit_match
-
+def digit_match(x, y):
+    """
+    :params:
+    - x, y (int): two numbers to compare digits
+    :returns:
+    - matches (int): number of matched digits between numbers
+    """
+    # ~~~ base cases ~~~
+    # check if x and y are 0
+    if x == 0 and y == 0:
+        return 1
+    # check if x and y are less than 10
+    elif x < 10 or y < 10:
+        # check if ones place digits match
+        if x % 10 == y % 10:
+            return 1
+        else:
+            # no match == 0
+            return 0
+    else:
+        # recursive case
+        if x % 10 == y % 10:
+            return 1 + digit_match(x//10, y //10)
+        else: 
+            return digit_match(x//10, y//10)
 
