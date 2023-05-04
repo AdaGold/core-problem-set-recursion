ce2cce9cfbfcaf0863aff636430a4e574500b5bb
Trang Tran <trantrang181197@gmail.com>
2023-05-03 20:19:34 -0700

    copy paste

--- part-2.py
+++ part-2.py
@@ -4,13 +4,31 @@
 # the appropriate comment.
 
 # search
-
-
-
+def search(array, querry):
+    if not array:
+        return False
+    if querry == array[0]:
+        return True
+    return search(array[1:], querry)
 # is_palindrome
-
-
-
+def is_palindrome(s):
+    if not s: # If the input string is empty, return False
+        return False
+    if len(s) <= 2 and s[0] == s[-1]:
+        return True
+    if s[0] != s[-1]:
+        return False
+    return is_palindrome(s[1:-1])
 # digit_match
-
-
+def digit_match(int1, int2):
+    if int1 == int2:
+        return len(str(int1))
+    if int1 < 10 and int2 < 10 and int1 != int2:
+        return 0
+    if not int1 or not int2:
+        return 0
+    if int1 % 10 == int2 % 10:
+        count = 1
+    else:
+        count = 0
+    return count + digit_match(int1 // 10, int2 // 10)
