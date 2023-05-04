3964856e93668ba086795eff477b3d66c8f459cd
dreinicke27 <danicareinicke@gmail.com>
2023-05-04 13:22:54 -0700

    completed part 1 and most of part 2

--- part-1.py
+++ part-1.py
@@ -4,17 +4,42 @@
 # the appropriate comment.
 
 # factorial
-
+def factorial(n):
+    if n < 0:
+        raise ValueError
+        
+    # Base case n = 1
+    if n == 0:
+        return 1
+    
+    return factorial(n-1) * n
 
 
 # reverse
-
+def reverse(text):
+    if len(text) == 0:
+        return text
+    
+    return reverse(text[1:]) + text[0]
+    
 
 
 # bunny
-
+def bunny(count):
+    if count == 0:
+        return 0
+        
+    return bunny(count - 1) + 2
 
 
 # is_nested_parens
-
+def is_nested_parens(parens):
+    if len(parens) % 2 != 0:
+        return False 
+    
+    if len(parens) == 0:
+        return True
+    
+    if len(parens) > 1 and parens[0] == "(" and parens[-1] == ")":
+        return is_nested_parens(parens[1:-1])
 
--- part-2.py
+++ part-2.py
@@ -4,11 +4,24 @@
 # the appropriate comment.
 
 # search
-
+def search(array, query):
+    if len(array) == 0:
+        return False
+    
+    if array[0] == query:
+        return True
+    
+    return search(array[1:],query)
 
 
 # is_palindrome
-
+def is_palindrome(text):
+    if len(text) <= 1:
+        return True
+    if len(text) >= 2 and text[0] == text[-1]:
+        return is_palindrome(text[1:-1])
+    else:
+        return False
 
 
 # digit_match
