c8d21e41f2b6563e40d8e7ce8e6a37ea1f677763
Sydney Alberson <sydney.alberson@gmail.com>
2023-05-08 11:30:31 -0400

    Completed some of Problem Set Part 2

--- part-2.py
+++ part-2.py
@@ -4,11 +4,24 @@
 # the appropriate comment.
 
 # search
-
+def search(array, query):
+    if not array:
+        return False
+    elif query == array[-1]:
+        return True
+    
+    return search(array[:-1], query)
 
 
 # is_palindrome
-
+def is_palindrome(text):
+    if len(text) < 2:
+        if text[0] == text[-1]:
+            return True
+    elif len(text) > 2:
+        return is_palindrome(text[1:-1])
+    
+    return False
 
 
 # digit_match
