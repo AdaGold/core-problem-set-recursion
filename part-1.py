4bed64f676f539d1d96a99b1050f03b1392de061
Pha'lesa Patton <ppatton86@gmail.com>
2023-05-08 14:50:28 -0400

    Problem Set 1 Solutions

--- part-1.py
+++ part-1.py
@@ -4,17 +4,54 @@
 # the appropriate comment.
 
 # factorial
+def factorial(n):
+    if n == 0:
+        return 1
+    
+    if n < 0:
+        raise ValueError 
+    
+    return n * factorial(n-1)
+
 
 
 
 # reverse
+def reverse(text):
+    if len(text) <= 1:
+        return text
+        
+    
+    
+    return reverse(text[1::]) + text[0]
 
 
 
 # bunny
+def bunny(count):
+    if count <= 0:
+        return 0
+    else:
+        return bunny(count-1) + 2
+    
 
 
 
 # is_nested_parens
+#I realized I wasn't supposed to do it this way
+#I understood the proper solution after I saw it
 
+def is_nested_parens(parens):
+    paren_dict = {}
+    if len(parens) <= 1:
+        return True
+    for paren in parens:
+        if paren not in paren_dict:
+            paren_dict[paren] = 1
+        else:
+            paren_dict[paren] +=1
+    if list(paren_dict.values())[0] == list(paren_dict.values())[1]:
+        return True
+    else:
+        return False
 
