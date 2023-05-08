056c5a5ee77dcde710a2d39aff38f2a3415c2328
Kat <105607428+thekatsmeowkode@users.noreply.github.com>
2023-05-08 13:45:21 -0400

    Update part-1.py

--- part-1.py
+++ part-1.py
@@ -4,17 +4,34 @@
 # the appropriate comment.
 
 # factorial
-
+def factorial(n):
+    if n < 0:
+        raise ValueError
+    if n == 0:
+        return 1
+    return n * factorial(n-1)
 
 
 # reverse
-
+def reverse(text):
+    if len(text) == 0:
+       return text
+    return reverse(text[1:]) + text[0]
 
 
 # bunny
-
+def bunny(count):
+    if count == 0:
+        return count
+    else:
+        return 2 + bunny(count -1)
 
 
 # is_nested_parens
-
+def is_nested_parens(parens):
+    if len(parens) < 2:
+        return True
+    if parens[0] != "(" or parens[-1] != ")":
+        return False
+    return is_nested_parens(parens[1:-1])
 
