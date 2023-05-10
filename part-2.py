a6bdde7e016e5d8a2622b877a91cd6858bb0474b
Jennifer Tam <80274896+JenniferWTam@users.noreply.github.com>
2023-05-10 11:23:07 -0700

    Update part-2.py

--- part-2.py
+++ part-2.py
@@ -28,16 +28,29 @@ def is_palindrome(text):
 
 
 # digit_match
-# havent passed all the tests yet
 def digit_match(int1, int2):
     if int1 == 0 and int2 == 0:
-        return 1
+        return 1 
     elif int1 == 0 or int2 == 0:
         return 0
+    if int1 < 10 and int2 < 10:
+        if int1 == int2:
+            return 1
+        else:
+            return 0
     elif int1 % 10 == int2 % 10:
         return 1 + digit_match(int1 // 10, int2 // 10)
-    else:
-        return digit_match(int1 // 10, int2 // 10)
-        
+    return digit_match(int1 // 10, int2 // 10)
+    
+# def digit_match(int1, int2):
+#     if int1 < 10 or int2 < 10:
+#         if int1 % 10 == int2 % 10:
+#             return 1
+#         else:
+#             return 0
+#     if int1 % 10 == int2 % 10:
+#         return 1 + digit_match(int1 // 10, int2 // 10)
+#     else:
+#         return digit_match(int1 // 10, int2 // 10)
 
         
