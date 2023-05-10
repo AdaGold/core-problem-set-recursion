d684bc14b53a0898460c5dc5c66c0f085c7f12b2
Jennifer Tam <80274896+JenniferWTam@users.noreply.github.com>
2023-05-10 09:13:23 -0700

    Update part-2.py

--- part-2.py
+++ part-2.py
@@ -4,13 +4,40 @@
 # the appropriate comment.
 
 # search
+def search(array, query):
+    if not array or not query:
+        return False
+    
+    if array[0] == query:
+        return True
+        
+    return search(array[1:], query)
+    
 
 
 
 # is_palindrome
-
+def is_palindrome(text):
+    if len(text) <= 1:
+        return True
+        
+    elif text[0] == text[-1]:
+        return is_palindrome(text[1:-1])
+    
+    return False
 
 
 # digit_match
+# havent passed all the tests yet
+def digit_match(int1, int2):
+    if int1 == 0 and int2 == 0:
+        return 1
+    elif int1 == 0 or int2 == 0:
+        return 0
+    elif int1 % 10 == int2 % 10:
+        return 1 + digit_match(int1 // 10, int2 // 10)
+    else:
+        return digit_match(int1 // 10, int2 // 10)
+        
 
-
+        
