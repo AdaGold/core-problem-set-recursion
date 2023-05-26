484edb8e69a058194c8a6f5671ff9c3db1045205
Ana Silvera <anasilverx@gmail.com>
2023-05-26 14:12:27 -0700

    added code for recursive functions problem set

--- part-2.py
+++ part-2.py
@@ -4,13 +4,31 @@
 # the appropriate comment.
 
 # search
-
-
+def search(array, query):
+    if not array or not isinstance(query, str):
+        return False
+    if query == array[0]:
+        return True
+    else:
+        return search(array[1:], query)
 
 # is_palindrome
-
-
+def is_palindrome(text):
+    if len(text) == 1:
+        return True
+    elif text[0] != text[-1]:
+        return False
+    else:
+        return is_palindrome(text[1:-1])
 
 # digit_match
-
-
+def digit_match(apples, oranges):
+    if apples < 10 or oranges < 10:
+        if apples % 10 == oranges % 10:
+            return 1
+        else:
+            return 0
+    if apples % 10 == oranges % 10:
+        return 1 + digit_match(apples//10, oranges//10)
+    else:
+        return digit_match(apples//10, oranges//10)
