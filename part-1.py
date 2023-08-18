de7ec11e870d1dce523cebdcfbb925a59f79c324
an2084 <angelanawang47@gmail.com>
2023-08-18 17:39:51 -0400

    Added all solutions

--- part-1.py
+++ part-1.py
@@ -4,17 +4,42 @@
 # the appropriate comment.
 
 # factorial
-
+def factorial(n):
+    try:
+        if n == 0:
+            return 1
+        return factorial(n-1) * n
+    except:
+        if n < 1:
+            raise ValueError
 
 
 # reverse
-
+def reverse(text):
+    print("Text:",text)
+    if len(text) == 1 or not text:
+        return text
+    
+    return (text[-1] + reverse(text[:-1]))
 
 
 # bunny
-
+def bunny(count):
+    if count == 0:
+        return 0
+    return bunny(count-1) + 2
 
 
 # is_nested_parens
-
-
+def is_nested_parens(parens):
+    if not parens:
+        return True
+    if len(parens) == 1:
+        return False
+    if len(parens) == 2:
+        if parens[0] == ")" and parens[1] == "(":
+            return False
+        if parens[0] == parens[1]:
+            return False
+        
+    return is_nested_parens(parens[1:-1])
--- part-2.py
+++ part-2.py
@@ -4,13 +4,40 @@
 # the appropriate comment.
 
 # search
-
+def is_nested_parens(parens):
+    if not parens:
+        return True
+    if len(parens) == 1:
+        return False
+    if len(parens) == 2:
+        if parens[0] == ")" and parens[1] == "(":
+            return False
+        if parens[0] == parens[1]:
+            return False
+        
+    return is_nested_parens(parens[1:-1])
 
 
 # is_palindrome
-
+def is_palindrome(text):
+    if len(text) <= 1:
+        return True
+    if text[0] == text[-1]:
+        return is_palindrome(text[1:-1])
+    return False
 
 
 # digit_match
-
+def digit_match(x,y):
+    x=str(x)
+    y=str(y)
+    count=0
+    if len(x) == 0 or len(y) == 0:
+        return count
+    if x[-1] == y[-1]:
+        count += 1
+        count += digit_match(x[:-1], y[:-1])
+    elif x[-1] != y[-1]:
+        count += digit_match(x[:-1], y[:-1])
+    return count
 
